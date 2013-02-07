f68096e4b171dc704252a1200539a0026db0910b
unknown <Rafael@.(none)>
2013-02-06 23:27:59 -0430

    Registro y Mi Cuenta

--- protected/modules/user/views/profileField/create.php
+++ protected/modules/user/views/profileField/create.php
@@ -8,5 +8,15 @@ $this->menu=array(
     array('label'=>UserModule::t('Manage Users'), 'url'=>array('/user/admin')),
 );
 ?>
+<div class="container margin_top">
+  <div class="row">
+    <div class="span6 offset3">
+      <h1>Registro</h1>
+      <article class="bg_color3 margin_top  margin_bottom_small padding_small box_1">
 <h1><?php echo UserModule::t('Create Profile Field'); ?></h1>
 <?php echo $this->renderPartial('_form', array('model'=>$model)); ?>
+
+      </article>
+    </div>
+  </div>
+</div>

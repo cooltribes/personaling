70884f65961c6a06690b8f2647f446bba8e8e321
unknown <Rafael@.(none)>
2013-02-11 23:43:02 -0430

    Controlador y Vista para tienda

---
+++ protected/views/tienda/index.php
@@ -0,0 +1,55 @@
+<?php
+/* @var $this TiendaController */
+
+$this->breadcrumbs=array(
+	'Tienda',
+);
+?>
+<div class="container margin_top">
+  <div class="row">
+    <div class="span3">
+      <div class="shadow_1">
+        <select  class="span3">
+          <option>Buscar por Categoria</option>
+          <option>Categoria 1</option>
+          <option>Categoria 2</option>
+          <option>Categoria 3</option>
+        </select>
+        <input type="text" placeholder="Buscar por palabras clave"  class="span3">
+        <a href="#" title="Buscar" class="btn">Buscar</a>
+        <hr/>
+        <div class="row tienda_iconos">
+          <div class="span1"><img src="http://placehold.it/70x70" />Zapatos</div>
+          <div class="span1"><img src="http://placehold.it/70x70" />Accesorios</div>
+          <div class="span1"><img src="http://placehold.it/70x70" />Pantalones</div>
+          <div class="span1"><img src="http://placehold.it/70x70" />Camisas</div>
+          <div class="span1"><img src="http://placehold.it/70x70" />Franelas</div>
+          <div class="span1"><img src="http://placehold.it/70x70" />Tops</div>
+          <div class="span1"><img src="http://placehold.it/70x70" />Zapatos</div>
+        </div>
+        <hr/>
+        <h5>Buscar por colores</h5>
+        <div class="clearfix tienda_colores"> <img src="http://placehold.it/30x30" /> <img src="http://placehold.it/30x30" /> <img src="http://placehold.it/30x30" /> <img src="http://placehold.it/30x30" /> <img src="http://placehold.it/30x30" /> <img src="http://placehold.it/30x30" /> <img src="http://placehold.it/30x30" /> <img src="http://placehold.it/30x30" /> <img src="http://placehold.it/30x30" /> <img src="http://placehold.it/30x30" /> <img src="http://placehold.it/30x30" /> <img src="http://placehold.it/30x30" /> <img src="http://placehold.it/30x30" /> <img src="http://placehold.it/30x30" /> <img src="http://placehold.it/30x30" /> <img src="http://placehold.it/30x30" /> <img src="http://placehold.it/30x30" /> <img src="http://placehold.it/30x30" /> <img src="http://placehold.it/30x30" /> <img src="http://placehold.it/30x30" /> <img src="http://placehold.it/30x30" /> </div>
+        <hr/>
+        <h5>Looks con estas prendas:</h5>
+        <img src="http://placehold.it/270x200" /> </div>
+    </div>
+    <div class="span9 tienda_productos">
+      <div class="row">
+        <article class="span3"> <img src="http://placehold.it/270x320"/> </article>
+        <article class="span3"> <img src="http://placehold.it/270x320"/> </article>
+        <article class="span3"> <img src="http://placehold.it/270x320"/> </article>
+        <article class="span3"> <img src="http://placehold.it/270x320"/> </article>
+        <article class="span3"> <img src="http://placehold.it/270x320"/> </article>
+        <article class="span3"> <img src="http://placehold.it/270x320"/> </article>
+        <article class="span3"> <img src="http://placehold.it/270x320"/> </article>
+        <article class="span3"> <img src="http://placehold.it/270x320"/> </article>
+        <article class="span3"> <img src="http://placehold.it/270x320"/> </article>
+        <article class="span3"> <img src="http://placehold.it/270x320"/> </article>
+        <article class="span3"> <img src="http://placehold.it/270x320"/> </article>
+        <article class="span3"> <img src="http://placehold.it/270x320"/> </article>
+
+      </div>
+    </div>
+  </div>
+</div>

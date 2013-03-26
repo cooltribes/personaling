928b8f9cddf7091f2b26b951a60ee3c1404d394e
juanr Pernia <juanrules@gmail.com>
2013-03-26 18:46:35 -0430

    Integraciones
    Se integro al Yii:
    EL login
    EL home
    El recovery
    y el register

--- protected/modules/user/views/recovery/recovery.php
+++ protected/modules/user/views/recovery/recovery.php
@@ -4,30 +4,20 @@ $this->breadcrumbs=array(
 	UserModule::t("Restore"),
 );
 ?>
-
+<div class="span6 offset3">
 <h1><?php echo UserModule::t("Restore"); ?></h1>
-
 <?php if(Yii::app()->user->hasFlash('recoveryMessage')): ?>
-<div class="success">
-<?php echo Yii::app()->user->getFlash('recoveryMessage'); ?>
-</div>
+<div class="success"> <?php echo Yii::app()->user->getFlash('recoveryMessage'); ?> </div>
 <?php else: ?>
-
-<div class="form">
-<?php echo CHtml::beginForm(); ?>
-
-	<?php echo CHtml::errorSummary($form); ?>
-	
-	<div class="row">
-		<?php echo CHtml::activeLabel($form,'login_or_email'); ?>
-		<?php echo CHtml::activeTextField($form,'login_or_email') ?>
-		<p class="hint"><?php echo UserModule::t("Please enter your login or email addres."); ?></p>
-	</div>
-	
-	<div class="row submit">
-		<?php echo CHtml::submitButton(UserModule::t("Restore")); ?>
-	</div>
-
-<?php echo CHtml::endForm(); ?>
-</div><!-- form -->
+<section class="bg_color3 margin_top  margin_bottom_small padding_small box_1">
+  <div class="form personaling_form"> <?php echo CHtml::beginForm(); ?> <?php echo CHtml::errorSummary($form); ?>
+    <div class="control-group"> <?php echo CHtml::activeLabel($form,'login_or_email'); ?>
+      <div class="controls"><?php echo CHtml::activeTextField($form,'login_or_email', array('class'=>'span5',)) ?>
+        <p class="hint"><?php echo UserModule::t("Please enter your login or email addres."); ?></p>
+      </div>
+    </div>
+    <div class="form-actions"> <?php echo CHtml::submitButton(UserModule::t("Restore"), array('class'=>'btn btn-danger btn-large')); ?> </div>
+    <?php echo CHtml::endForm(); ?> </div>
+  <!-- form --> 
+</section></div>
 <?php endif; ?>

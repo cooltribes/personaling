f71dffecab5592cf2fb5abacc077c30970bfe9e8
juanr Pernia <juanrules@gmail.com>
2013-03-27 12:58:22 -0430

    Integrando el "tu tipo" del prototipo con el Yii
    Sa Cambiaron los radiobuttons por <a> para que rafa capte la eleccion
    del usuario por jQuery mas facil (o algo asi fue lo que entendi)

--- protected/modules/user/views/profile/tutipo.php
+++ protected/modules/user/views/profile/tutipo.php
@@ -1,10 +1,10 @@
-<?php $this->pageTitle=Yii::app()->name . ' - '.UserModule::t("Profile");
+<?php /*?><?php $this->pageTitle=Yii::app()->name . ' - '.UserModule::t("Profile");
 $this->breadcrumbs=array(
 	UserModule::t("Mi cuenta")=>array('micuenta'),
 	UserModule::t("Tu perfil corporal"),
 );
 ?>
-
+<?php */?>
 <div class="container margin_top">
   <div class="row">
     <div class="span12">
@@ -43,7 +43,7 @@ $this->widget('bootstrap.widgets.TbAlert', array(
 		</div>
      <!-- MENU OFF -->
      <?php } ?>   
-      <h1>Tu tipo</h1>
+      <h1>Tu tipo <small> - Escoge las opciones que más se parezcan a ti:</small></h1>
    
       <article class="margin_top  margin_bottom_small ">
 <?php $form=$this->beginWidget('bootstrap.widgets.TbActiveForm', array(
@@ -57,42 +57,42 @@ $this->widget('bootstrap.widgets.TbAlert', array(
 	),
 )); ?>
           <fieldset>
-            <legend class="text_align_center">Lorem ipsum: </legend>
+            <legend>Características básicas: </legend>
             <div class="control-group">
               <div class="controls row">
-                <div class="span3">
+                <div class="span2 offset1">
                   <?php 
                   	$field = ProfileField::model()->findByAttributes(array('varname'=>'altura'));
-				  	echo $form->dropDownListRow($profile,$field->varname,Profile::range($field->range));
+				  	echo $form->dropDownListRow($profile,$field->varname,Profile::range($field->range), array('class'=>'span2'));
                   ?>
                 </div> 
-                <div class="span3">
+                <div class="span2">
                   <?php 
                   	$field = ProfileField::model()->findByAttributes(array('varname'=>'contextura'));
-				  	echo $form->dropDownListRow($profile,$field->varname,Profile::range($field->range));
+				  	echo $form->dropDownListRow($profile,$field->varname,Profile::range($field->range), array('class'=>'span2'));
                   ?>
                 </div>
-                <div class="span3">
+                <div class="span2">
                   <?php 
                   	$field = ProfileField::model()->findByAttributes(array('varname'=>'pelo'));
-				  	echo $form->dropDownListRow($profile,$field->varname,Profile::range($field->range));
+				  	echo $form->dropDownListRow($profile,$field->varname,Profile::range($field->range), array('class'=>'span2'));
                   ?>
                 </div>
-                <div class="span3">
+                <div class="span2">
                   <?php 
                   	$field = ProfileField::model()->findByAttributes(array('varname'=>'ojos'));
-				  	echo $form->dropDownListRow($profile,$field->varname,Profile::range($field->range));
+				  	echo $form->dropDownListRow($profile,$field->varname,Profile::range($field->range), array('class'=>'span2'));
                   ?>
                 </div>
-                <div class="span3">
+                <div class="span2">
                   <?php 
                   	$field = ProfileField::model()->findByAttributes(array('varname'=>'piel'));
-				  	echo $form->dropDownListRow($profile,$field->varname,Profile::range($field->range));
+				  	echo $form->dropDownListRow($profile,$field->varname,Profile::range($field->range), array('class'=>'span2'));
                   ?>
                 </div>                
               </div>
             </div>
-            <legend class="text_align_center">Tu forma de cuerpo: </legend>
+              <legend class="margin_top">Forma de tu cuerpo</legend>
             <div class="control-group">
               <div class="controls row">
                 <!--
@@ -122,9 +122,41 @@ $this->widget('bootstrap.widgets.TbAlert', array(
                 </div>
                 -->
                 <?php 
-                $field = ProfileField::model()->findByAttributes(array('varname'=>'tipo_cuerpo'));
-				echo $form->radioButtonListInlineRow($profile,$field->varname,Profile::range($field->range));
+              //  $field = ProfileField::model()->findByAttributes(array('varname'=>'tipo_cuerpo'));
+			  // echo $form->radioButtonListInlineRow($profile,$field->varname,Profile::range($field->range));
 				?>
+                <ul class="thumbnails">
+                <li class="span3"> <a href="#" title="Elegir este tipo de cuerpo">
+                  <div class="thumbnail"> <img alt="Tipo de cuerpo" style="width: 270px; height: 400px;" src="http://placehold.it/270x400">
+                    <div class="caption text_align_center CAPS">
+                      <p>Cras justoelit.</p>
+                    </div>
+                  </div>
+                  </a> </li>
+                <li class="span3"> <a href="#" title="Elegir este tipo de cuerpo">
+                  <div class="thumbnail"> <img alt="Tipo de cuerpo" style="width: 270px; height: 400px;" src="http://placehold.it/270x400">
+                    <div class="caption text_align_center CAPS">
+                      <p>Cras justoelit.</p>
+                    </div>
+                  </div>
+                  </a> </li>
+                <li class="span3"> <a href="#" title="Elegir este tipo de cuerpo">
+                  <div class="thumbnail"> <img alt="Tipo de cuerpo" style="width: 270px; height: 400px;" src="http://placehold.it/270x400">
+                    <div class="caption text_align_center CAPS">
+                      <p>Cras justoelit.</p>
+                    </div>
+                  </div>
+                  </a> </li>
+                <li class="span3"> <a href="#" title="Elegir este tipo de cuerpo">
+                  <div class="thumbnail"> <img alt="Tipo de cuerpo" style="width: 270px; height: 400px;" src="http://placehold.it/270x400">
+                    <div class="caption text_align_center CAPS">
+                      <p>Cras justoelit.</p>
+                    </div>
+                  </div>
+                  </a> </li>
+              </ul>
+                
+                
               </div>
             </div>
             <div class="form-actions">             			
@@ -133,6 +165,8 @@ $this->widget('bootstrap.widgets.TbAlert', array(
 						    'label'=>isset($editar)?'Guardar':'Siguiente',
 						    'type'=>'danger', // null, 'primary', 'info', 'success', 'warning', 'danger' or 'inverse'
 						    'size'=>'large', // null, 'large', 'small' or 'mini'
+							'htmlOptions' => array('class'=>'pull-right'), 
+							
 						)); ?> 
 			</div>
           </fieldset>

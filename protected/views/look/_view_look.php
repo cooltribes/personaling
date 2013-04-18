f06acd31e3625a8cfd8b2fcfe650f9c447ad7795
unknown <Rafael@.(none)>
2013-04-18 17:23:28 -0430

    Mostrar precios en administrador de looks

--- protected/views/look/_view_look.php
+++ protected/views/look/_view_look.php
@@ -5,7 +5,7 @@
                 <strong>Nro. Items</strong>: <?php echo $data->countItems(); ?></td>
             <td><strong>P.S.</strong>: <?php echo $data->user->profile->first_name; ?><br/>
                 <strong>Marcas</strong>: Mango, Suite Blanco, Aldo, Accessorize y Desigual </td>
-            <td>650,00</td>
+            <td><?php echo $data->getPrecio(); ?></td>
             <td>10</td>
             <td>6500,00</td>
             <td>Por aprobar</td>

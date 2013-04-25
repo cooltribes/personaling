7b0b6e1b7b40f13f53a53e515eb075defcf86be1
Rafael Palma <rpalma@upsidecorp.ch>
2013-04-25 13:13:40 -0430

    Adminsitracion de Usuarios

--- protected/modules/user/views/admin/index.php
+++ protected/modules/user/views/admin/index.php
@@ -47,6 +47,8 @@ $this->breadcrumbs=array(
     <div class="span2"><a href="#" class="btn btn-success">Crear usuario</a></div>
   </div>
     <hr/>
+   <?php
+$template = '{summary}
   <table width="100%" border="0" cellspacing="0" cellpadding="0" class="table table-bordered table-hover table-striped">
     <tr>
       <th rowspan="2" scope="col"><input name="Check" type="checkbox" value="Check"></th>
@@ -57,115 +59,44 @@ $this->breadcrumbs=array(
       <th rowspan="2" scope="col">Fecha de Registro</th>
       <th rowspan="2" scope="col"></th>
     </tr>
-    <tr>
+        <tr>
       <th scope="col">Pedidos</th>
       <th scope="col">Direcciones</th>
       <th scope="col">#</th>
       <th scope="col">Ultima Fecha</th>
     </tr>
-    <tr>
-      <td><input name="Check" type="checkbox" value="Check"></td>
-      <td><img src="images/kitten.png" width="70" height="70" alt="avatar"></td>
-      <td><strong> <span class="CAPS">Scott Pilgrim</span></strong><br/>
-        <strong>ID</strong>: 0001<br/>
-        Personal Shopper </td>
-      <td><small><strong>eMail</strong>: keniadubay@gmail.com<br/>
-        <strong>Telefono</strong>: 0424-709.85.06 <br/>
-        <strong>Ciudad</strong>: San Cristobal 
-        
-     </small>
-        
-        </td>
-      <td>0</td>
-      <td>1</td>
-      <td>300.00 Bs.</td>
-      <td>25</td>
-      <td>Ene. 11, 2013</td>
-      <td>Dic 01, 2012</td>
-      <td>
-      <div class="dropdown"> <a class="dropdown-toggle btn btn-small" id="dLabel" role="button" data-toggle="dropdown" data-target="#" href="admin_pedidos_detalles.php"> <i class="icon-cog"></i></a> 
-          <!-- Link or button to toggle dropdown -->
-          <ul class="dropdown-menu" role="menu" aria-labelledby="dLabel">
-            <li><a title="ver" href="#">  <i class="icon-eye-open">  </i>  Ver</a></li>
-      <li><a title="Editar" href="admin_editar_usuario.php">  <i class="icon-edit">  </i>  Editar</a></li>
-      <li><a title="Cambiar contraseña" href="#">  <i class="icon-lock">  </i>  Cambiar contraseña</a></li>
-      <li><a title="Reenviar invitacion" href="#">  <i class="icon-refresh">  </i>  Reenviar invitacion</a></li>
-      <li><a title="Cargar Saldo" href="#">  <i class="icon-gift">  </i>  Cargar Saldo</a>
-            <li class="divider"></li>
-      <li><a title="Eliminar" href="#">  <i class="icon-trash">  </i>  Eliminar</a></li>
-          </ul>
-        </div>
-          
-      </td>
-    </tr>
-    <tr>
-      <td><input name="Check" type="checkbox" value="Check"></td>
-      <td><img src="images/hipster_girl.jpg" width="70" height="70" alt="avatar"></td>
-      <td><strong> <span class="CAPS">John Snow</span></strong><br/>
-        <strong>ID</strong>: 0001<br/>
-        Personal Shopper </td>
-      <td><small><strong>eMail</strong>: keniadubay@gmail.com<br/>
-        <strong>Telefono</strong>: 0424-709.85.06 <br/>
-        <strong>Ciudad</strong>: San Cristobal </small></td>
-      <td>0</td>
-      <td>1</td>
-      <td>300.00 Bs.</td>
-      <td>25</td>
-      <td>Ene. 11, 2013</td>
-      <td>Dic 01, 2012</td>
-      <td> <div class="dropdown"> <a class="dropdown-toggle btn btn-small" id="dLabel" role="button" data-toggle="dropdown" data-target="#" href="admin_pedidos_detalles.php"> <i class="icon-cog"></i></a> 
-          <!-- Link or button to toggle dropdown -->
-          <ul class="dropdown-menu" role="menu" aria-labelledby="dLabel">
-            <li><a title="ver" href="#">  <i class="icon-eye-open">  </i>  Ver</a></li>
-      <li><a title="Editar" href="admin_editar_usuario.php">  <i class="icon-edit">  </i>  Editar</a></li>
-      <li><a title="Cambiar contraseña" href="#">  <i class="icon-lock">  </i>  Cambiar contraseña</a></li>
-      <li><a title="Reenviar invitacion" href="#">  <i class="icon-refresh">  </i>  Reenviar invitacion</a></li>
-      <li><a title="Cargar Saldo" href="#">  <i class="icon-gift">  </i>  Cargar Saldo</a>
-            <li class="divider"></li>
-      <li><a title="Eliminar" href="#">  <i class="icon-trash">  </i>  Eliminar</a></li>
-          </ul>
-        </div></td>
-    </tr>
-    <tr>
-      <td><input name="Check" type="checkbox" value="Check"></td>
-      <td><img src="images/kitten.png" width="70" height="70" alt="avatar"></td>
-      <td><strong> <span class="CAPS">Lorem Ipsum</span></strong><br/>
-        <strong>ID</strong>: 0001<br/>
-        Personal Shopper </td>
-      <td><small><strong>eMail</strong>: keniadubay@gmail.com<br/>
-        <strong>Telefono</strong>: 0424-709.85.06 <br/>
-        <strong>Ciudad</strong>: San Cristobal </small></td>
-      <td>0</td>
-      <td>1</td>
-      <td>300.00 Bs.</td>
-      <td>25</td>
-      <td>Ene. 11, 2013</td>
-      <td>Dic 01, 2012</td>
-      <td> <div class="dropdown"> <a class="dropdown-toggle btn btn-small" id="dLabel" role="button" data-toggle="dropdown" data-target="#" href="admin_pedidos_detalles.php"> <i class="icon-cog"></i></a> 
-          <!-- Link or button to toggle dropdown -->
-          <ul class="dropdown-menu" role="menu" aria-labelledby="dLabel">
-            <li><a title="ver" href="#">  <i class="icon-eye-open">  </i>  Ver</a></li>
-      <li><a title="Editar" href="admin_editar_usuario.php">  <i class="icon-edit">  </i>  Editar</a></li>
-      <li><a title="Cambiar contraseña" href="#">  <i class="icon-lock">  </i>  Cambiar contraseña</a></li>
-      <li><a title="Reenviar invitacion" href="#">  <i class="icon-refresh">  </i>  Reenviar invitacion</a></li>
-      <li><a title="Cargar Saldo" href="#">  <i class="icon-gift">  </i>  Cargar Saldo</a>
-            <li class="divider"></li>
-      <li><a title="Eliminar" href="#">  <i class="icon-trash">  </i>  Eliminar</a></li>
-          </ul>
-        </div></td>
-    </tr>
-  </table>
-  <div class="pagination pagination-right">
-    <ul>
-      <li class="disabled"><a href="#">&laquo;</a></li>
-      <li class="active"><a href="#">1</a></li>
-      <li><a href="#">2</a></li>
-      <li><a href="#">3</a></li>
-      <li><a href="#">4</a></li>
-      <li><a href="#">5</a></li>
-      <li><a href="#">Next</a></li>
-    </ul>
-  </div>
+    {items}
+    </table>
+    {pager}
+	';
+
+		$this->widget('zii.widgets.CListView', array(
+	    'id'=>'list-user',
+	    'dataProvider'=>$model->search(),
+	    'itemView'=>'_view_user',
+	    'template'=>$template,
+/*	    'afterAjaxUpdate'=>" function(id, data) {
+						    	
+							$('#todos').click(function() { 
+				            	inputs = $('table').find('input').filter('[type=checkbox]');
+				 
+				 				if($(this).attr('checked')){
+				                     inputs.attr('checked', true);
+				               	}else {
+				                     inputs.attr('checked', false);
+				               	} 	
+							});
+						   
+							} ",
+*/		'pager'=>array(
+			'header'=>'',
+			'htmlOptions'=>array(
+			'class'=>'pagination pagination-right',
+		)
+		),					
+	));    
+	?> 
+	
   <hr/>
    <div class="row">
    <div class="span3"><select class="span3">

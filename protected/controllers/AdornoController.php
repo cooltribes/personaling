028d618bbeb29f2fb6d13d12bdca7e58ff368359
unknown <Rafael@.(none)>
2013-05-31 06:31:30 -0430

    Adornos

--- protected/controllers/AdornoController.php
+++ protected/controllers/AdornoController.php
@@ -33,4 +33,26 @@ class AdornoController extends Controller
 		);
 	}
 	*/
+	
+		public function actionGetImage($id)
+	{
+		$model = $this->loadModel($id);
+		//$image_url = $model->getImageUrl(array('type'=>'thumb','ext'=>'png')); <--- este caso para cuando exista thumb
+		$image_url = $model->getImageUrl(array('ext'=>'png'));
+		list($width, $height, $type, $attr) = getimagesize(Yii::getPathOfAlias('webroot').'/..'.$image_url);		
+		echo '<div class="new" id="adorno'.$id.'">';
+		echo '<img '.$attr.' src="'.$image_url.'" alt>';
+		echo '<input type="hidden" name="adorno_id" value="'.$id.'">';
+		
+		echo '</div>';
+		//height="180" width="180"
+		
+	}
+		public function loadModel($id)
+	{
+		$model=Adorno::model()->findByPk($id);
+		if($model===null)
+			throw new CHttpException(404,'The requested page does not exist.');
+		return $model;
+	}
 }

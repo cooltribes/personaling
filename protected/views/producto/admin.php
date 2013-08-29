bc7e3aa70ec588cbe9f2ab0696f0e2ec3a507d5c
Rafael Palma <rpalma@upsidecorp.ch>
2013-08-29 12:09:53 -0430

    Bug

--- protected/views/producto/admin.php
+++ protected/views/producto/admin.php
@@ -288,14 +288,14 @@ $template = '{summary}
 function compara_fechas($fecha1,$fecha2)
 {
       if (preg_match("/[0-9]{1,2}\/[0-9]{1,2}\/([0-9][0-9]){1,2}/",$fecha1))
-              list($dia1,$mes1,$año1)=split("/",$fecha1);
+              list($dia1,$mes1,$año1)=explode("/",$fecha1);
       if (preg_match("/[0-9]{1,2}-[0-9]{1,2}-([0-9][0-9]){1,2}/",$fecha1))
-              list($dia1,$mes1,$año1)=split("-",$fecha1);
+              list($dia1,$mes1,$año1)=explode("-",$fecha1);
       if (preg_match("/[0-9]{1,2}\/[0-9]{1,2}\/([0-9][0-9]){1,2}/",$fecha2))
-              list($dia2,$mes2,$año2)=split("/",$fecha2);
+              list($dia2,$mes2,$año2)=explode("/",$fecha2);
       if (preg_match("/[0-9]{1,2}-[0-9]{1,2}-([0-9][0-9]){1,2}/",$fecha2))
-              list($dia2,$mes2,$año2)=split("-",$fecha2);
-        $dif = mktime(0,0,0,$mes1,$dia1,$año1) - mktime(0,0,0, $mes2,$dia2,$año2);
+              list($dia2,$mes2,$año2)=explode("-",$fecha2);
+        $dif = mktime(0,0,0,$mes1,$dia1,$año1) - mktime(0,0,0, $mes2,$dia2,$año2); 
         return ($dif);
 }
 ?>

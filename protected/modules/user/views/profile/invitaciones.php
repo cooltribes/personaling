b3bc6eb37db244bd3d0361a761e7b0add50401e3
Carlos Linares <clinares@upsidecorp.ch>
2013-09-02 12:35:58 -0430

    Cambiado copy de formas de pago y condiciones de envios
    Cambiado estilo de invitaciones
    Resuelto bug de estilo en crear look

--- protected/modules/user/views/profile/invitaciones.php
+++ protected/modules/user/views/profile/invitaciones.php
@@ -111,7 +111,8 @@ $create_date = date('j M Y', $create_time);
                                             array(
                                                 'model' => $model,
                                                 'field' => 'emailList',
-                                                'form' => $form,                                                
+                                                'form' => $form, 
+                                                 'cssEntry' => "background-color: #ad3682; color: white;  display: inline-block;"                                               
                                             )
                                     );
                                     ?>

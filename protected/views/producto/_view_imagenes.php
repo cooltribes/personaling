db7b7d9a497706e9d694ab26c2667009a0a634e8
Carlos Linares <clinares@upsidecorp.ch>
2013-09-19 17:50:07 -0430

    Colocado nombre del producto en toda la edicion en Admin

--- protected/views/producto/_view_imagenes.php
+++ protected/views/producto/_view_imagenes.php
@@ -85,6 +85,7 @@ $this->breadcrumbs=array(
 <div class="container margin_top">
   <div class="page-header">
     <h1>Editar Producto - Imágenes</small></h1>
+    <h2><?php echo $model->nombre; ?></h2>
   </div>
   <!-- SUBMENU ON --> 
   

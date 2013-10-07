ba4a5e784058f91ec3f0130ae23056c556f2e04c
Daniel Duque <dduque@upsidecorp.ch>
2013-10-07 18:19:22 -0430

    Cantidad de looks vendidos por ps

--- protected/views/campana/_view_ps.php
+++ protected/views/campana/_view_ps.php
@@ -1,5 +1,12 @@
 <?php
 $campana_ps = CampanaHasPersonalShopper::model()->findByAttributes(array('campana_id'=>Yii::app()->session['campana_id'], 'user_id'=>$data->id));
+
+$looks = Look::model()->findAllByAttributes(array('user_id'=>$data->id));
+$contador = 0;
+
+$sql = "select count( distinct(a.tbl_orden_id) ) as total from tbl_orden_has_productotallacolor a, tbl_look b where a.look_id = b.id and b.user_id = ".$data->id;
+$total = Yii::app()->db->createCommand($sql)->queryScalar();
+
 ?>
 
 <tr <?php if($campana_ps) echo 'class="success"'; ?>>
@@ -8,5 +15,5 @@ $campana_ps = CampanaHasPersonalShopper::model()->findByAttributes(array('campan
 	<td><strong> <span class="CAPS"><?php echo $data->profile->first_name.' '.$data->profile->last_name; ?></span></strong><br>
 	<strong>ID</strong>: <?php echo $data->id; ?></td>
 	<td> <?php echo $data->email; ?> </td>
-	<td>500 </td>
+	<td><?php echo $total; ?></td>
 </tr>

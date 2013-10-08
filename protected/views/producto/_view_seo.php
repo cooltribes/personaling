e98daaa0612f09b52d05857a1e7dddf58f40f708
Daniel Duque <dduque@upsidecorp.ch>
2013-10-08 18:18:10 -0430

    cambio en geturl para look. Funcion en producto para el proximo id

--- protected/views/producto/_view_seo.php
+++ protected/views/producto/_view_seo.php
@@ -21,12 +21,12 @@ $this->breadcrumbs=array(
 )); ?>
 
 	<?php echo $form->errorSummary($model); ?>
-  
-  <?php
+<?php
 	echo CHtml::hiddenField('accion','def', array('id' => 'accion'));
-	//<input id="accion" type="hidden" value="" />	
+	
+	
 ?>
-  
+  <input id="id_sig" type="hidden" value="<?php echo $model->next($model->id); ?>" />	
   <div class="row margin_top">
     <div class="span9">
       <div class="bg_color3   margin_bottom_small padding_small box_1">
@@ -102,6 +102,7 @@ $this->breadcrumbs=array(
 			'htmlOptions' => array('id'=>'normal'),
 		)); ?>
        	<ul class="nav nav-stacked nav-tabs margin_top">
+       		<li><a id="siguiente" style="cursor: pointer" title="Guardar y Siguiente">Guardar y siguiente producto</a></li>
          	<li><a id="nuevo" style="cursor: pointer" title="Guardar y crear nuevo producto">Guardar y crear nuevo producto</a></li>
 			<li><a style="cursor: pointer" title="Restablecer" id="limpiar">Limpiar Formulario</a></li>
         </ul>
@@ -157,6 +158,23 @@ $this->breadcrumbs=array(
 		$('#producto-form').submit();
 	
 	}	
+	);
+	
+	$('a#siguiente').on('click', function(event) {
+		
+		event.preventDefault();
+		
+		$("#accion").attr("value", "siguiente");
+		var uno = $("#id_sig").attr("value");
+		
+		if(uno != ""){
+			//alert(uno);
+
+			// submit del form
+			$('#producto-form').submit();
+		}
+	}	
 	);	
+		
 		
 </script>

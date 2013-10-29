f046a0b26b5c75725477f317c11b5dff6184c392
nramirez <nramirez@upsidecorp.ch>
2013-10-29 12:10:32 -0430

    Mostrar personal shoppers destacadas en el TOP

--- protected/modules/user/views/admin/update.php
+++ protected/modules/user/views/admin/update.php
@@ -85,14 +85,19 @@ function getMonthsArray()
             <label for="" class="control-label ">Contraseña </label>
             <div class="controls">
               <input type="password" placeholder="Contraseña"  class="span5">
-              <div style="display:none" class="help-inline">ayuda aqui </div>
+              <div style="display:none" class="help-inline">Ingrese una contraseña</div>
             </div>
           </div>
           <div class="control-group"> <?php echo $form->dropDownListRow($model,'superuser',array(0=>'No',1=>'Si'),array('class'=>'span2')); ?> </div>
           <div class="control-group"> <?php echo $form->dropDownListRow($model,'personal_shopper',array(0=>'No', 1=>'Si', 2 => "Aplicante"),array('class'=>'span2')); ?> 
-              <?php if($model->personal_shopper == 1 && $model->ps_destacado){ ?>
+              <?php if($model->personal_shopper == 1){ ?>
               <span class="label label-warning" 
-                    style="margin-left: 180px;padding: 4px 10px;font-size: 12px;">Personal Shopper Destacado</span>
+                    style="margin-left: 180px;padding: 4px 10px;font-size: 12px;
+                    <?php echo ($model->ps_destacado)? '':'display:none;'; ?>">
+                  
+                    Personal Shopper Destacado
+                    
+              </span>
               <?php } ?>
           </div>
           <div class="control-group">

57b5a7b1667b6181a258e79371451f18fb785a70
Carlos Linares <clinares@upsidecorp.ch>
2013-11-05 15:38:10 -0430

    HTML para generar Gift Card

--- protected/views/giftcard/create.php
+++ protected/views/giftcard/create.php
@@ -4,15 +4,55 @@
 
 $this->breadcrumbs=array(
 	'Giftcards'=>array('index'),
-	'Create',
+	'Generar',
 );
 
-$this->menu=array(
-	array('label'=>'List Giftcard', 'url'=>array('index')),
-	array('label'=>'Manage Giftcard', 'url'=>array('admin')),
-);
 ?>
+<div class="container">
+	<h1>Gift Card</h1>
+	<section class="bg_color3  span9 offset1 margin_bottom_small padding_small box_1">
+		<form class="form-horizontal">
+		<fieldset>
+			<legend>Generar Gift Card</legend>
+			<div class="control-group">
+			<label class="control-label" for="inputMonto">Monto</label>
+			<div class="controls row-fluid">
+				<select class="span3">
+				  <option>300,00</option>
+				  <option>400,00</option>
+				  <option>500,00</option>
+				  <option>1000,00</option>
+				</select>
+			</div>
+			</div>
+	<!-- 	  <div class="control-group">
+		    <label class="control-label" for="inputPassword">Mensaje</label>
+		    <div class="controls">
+				<textarea rows="3" placeholder="Haz recibido un Gift Card de Personaling"></textarea>			
+		    </div>
+		  </div> -->
+			<div class="control-group">
+				<label class="control-label" for="inputMonto">Fecha Desde</label>	  	
+				<div class="controls row-fluid">
+	      			<input type="text" id="inputEmail" placeholder="Desde">
+				</div>							    
+			</div>
+			<div class="control-group">
+				<label class="control-label" for="inputMonto">Fecha Hasta</label>	  	
+				<div class="controls row-fluid">
+	      			<input type="text" id="inputEmail" placeholder="Hasta">
+				</div>							    
+			</div>	  
 
-<h1>Create Giftcard</h1>
+			<div class="control-group row">
+				<div class="controls pull-right">
 
-<?php echo $this->renderPartial('_form', array('model'=>$model)); ?>
+				  <button type="submit" class="btn btn-danger">Crear y enviar Gift Card</button>
+				</div>
+			</div>
+		</fieldset>
+		</form>
+
+
+	</section>
+</div>

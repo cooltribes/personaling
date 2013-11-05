618f8063fe8d66d434deaa3ba96a2756f0152a17
Carlos Linares <clinares@upsidecorp.ch>
2013-11-05 17:11:44 -0430

    HTML de enviar Gift Card

--- protected/views/giftcard/enviargiftcard.php
+++ protected/views/giftcard/enviargiftcard.php
@@ -12,10 +12,16 @@ $this->breadcrumbs=array(
 	<h1>Gift Card</h1>
 	<section class="bg_color3  span12 margin_bottom_small padding_medium box_1">
 
-		<form class="form-horizontal">
+		<form class="form-horizontal personaling_form ">
 		<fieldset>
 			<legend>Enviar Gift Card</legend>
+			<aside class="muted padding_small">
+				<span class="margin_right_medium ">Monto: 400,00 </span>
+				<span class="margin_right_medium ">Desde: 12/12/2013 </span>
+				<span class="margin_right_medium ">Hasta: 12/12/2014 </span>
+			</aside>
 			<div>
+				<p class="lead">1. Selecciona una Gift Card</p>
 				<ul class="thumbnails">
 					<li> <img src="http://placehold.it/200x100"> </li>
 					<li> <img src="http://placehold.it/200x100"> </li>
@@ -26,36 +32,37 @@ $this->breadcrumbs=array(
 
 
 			</div>	
-			<div class="row">	
-				<div class="span4">	
+			<div class="row margin_top">
+				<div class="span6">	
+					<p class="lead">2. Personalizala</p>	
 					<div class="control-group">
 						<label class="control-label" for="inputMonto">Para</label>
-						<div class="controls row-fluid">
-				      			<input type="text" id="inputPara" placeholder="Para">
+						<div class="controls row-fluid ">
+				      			<input class="input-xlarge" type="text" id="inputPara" placeholder="Para">
 						</div>
 					</div>
 					<div class="control-group">
 						<label class="control-label" for="inputMonto">Email</label>
 						<div class="controls row-fluid">
-				      			<input type="text" id="inputEmail" placeholder="Email">
+				      			<input class="input-xlarge" type="text" id="inputEmail" placeholder="Email">
 						</div>
 					</div>			
 			  		<div class="control-group">
 					    <label class="control-label" for="inputPassword">Mensaje</label>
 					    <div class="controls">
-							<textarea rows="3" placeholder="Haz recibido un Gift Card de Personaling"></textarea>			
+							<textarea class="input-xlarge" rows="3" placeholder="Haz recibido un Gift Card de Personaling"></textarea>			
 					    </div>
 				    </div>   
-					<div class="control-group row">
-						<div class="controls pull-right">
-						  <button type="submit" class="btn btn-danger">Crear Gift Card</button>
-						</div>
-					</div>
 				</div>	
-				<div class="span8">
+				<div class="span5">
 					<img src="http://placehold.it/450x250">
 				</div>
 			</div>
+			<div class="control-group row margin_top">
+				<div class="controls pull-right">
+				  <button type="submit" class="btn btn-danger">Enviar Gift Card</button>
+				</div>
+			</div>			
 		</fieldset>
 		</form>
 

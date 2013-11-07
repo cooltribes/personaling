660fa267ca2fcfe4004f51c5015f0140dbf18bb7
Carlos Linares <clinares@upsidecorp.ch>
2013-11-07 18:28:03 -0430

    Agregado Site Map

---
+++ protected/views/site/sitemap.php
@@ -0,0 +1,35 @@
+<section class="container margin_top">
+  <h1>Site Map</h1>
+  <ul class="margin_left_large ">
+      <li ><a href="http://personaling.com/">Home</a></li>
+
+      <li ><a href="http://personaling.com/site/comofunciona">Como Funciona</a></li>
+
+      <li > <a href="http://personaling.com/tienda/index">Tienda</a></li>
+
+      <li ><a href="http://personaling.com/magazine/"></a>Magazine</li>
+
+      <li ><a href="http://personaling.com/user/login">Login</a></li>
+
+      <li ><a href="http://personaling.com/user/registration">Registrate</a></li>
+
+      <li ><a href="http://personaling.com/site/formas_de_pago<">Formas de Pago</a></li>
+
+      <li > <a href="http://personaling.com/site/condiciones_de_envios_y_encomiendas">Envios y Encomiendas</a></li>
+
+      <li ><a href="http://personaling.com/site/politicas_de_devoluciones"> Politicas de Devoluciones</a></li>
+
+      <li ><a href="http://personaling.com/site/politicas_y_privacidad">Politicas y Privacidad</a></li>
+
+      <li ><a href="http://personaling.com/site/preguntas_frecuentes">Preguntas Frecuentes</a></li>
+
+      <li ><a href="http://personaling.com/site/terminos_de_servicio">Terminos de Servicio</a></li>
+
+      <li ><a href="http://personaling.com/site/acerca_de">Acerca de</a></li>
+
+      <li ><a href="http://personaling.com/site/contacto">Contacto</a></li>
+
+      <li ><a href="http://personaling.com/site/equipo_personaling">El Equipo de Personaling</a></li>
+
+  </ul>
+</section>

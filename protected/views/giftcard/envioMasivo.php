95d0dced73dfdf88dffa66dcd189b4d455d8b4d6
nramirez <nramirez@upsidecorp.ch>
2013-11-12 18:11:02 -0430

    Seleccionar usuarios para envio masivo

---
+++ protected/views/giftcard/envioMasivo.php
@@ -0,0 +1,85 @@
+<?php
+/* @var $this GiftcardController */
+/* @var $model Giftcard */
+
+
+$this->breadcrumbs=array(
+	'Giftcards'=>array('index'),
+	'Generar Masivo' =>array('createMasivo'),
+        'Seleccionar Usuarios' => array('seleccionarusuarios'),
+        'Envío Masivo',
+);
+
+?>
+<div class="container">
+    
+    <!-- FLASH ON --> 
+    <?php $this->widget('bootstrap.widgets.TbAlert', array(
+            'block'=>true, // display a larger alert block?
+            'fade'=>true, // use transitions?
+            'closeText'=>'&times;', // close link text - if set to false, no close link is displayed
+            'alerts'=>array( // configurations per alert type
+                'success'=>array('block'=>true, 'fade'=>true, 'closeText'=>'&times;'), // success, info, warning, error or danger
+                'error'=>array('block'=>true, 'fade'=>true, 'closeText'=>'&times;'), // success, info, warning, error or danger
+            ),
+        )
+    ); ?>	
+    <!-- FLASH OFF --> 
+	<h1>Gift Card</h1>
+	<section class="bg_color3  span12 margin_bottom_small padding_medium box_1">
+                <?php $form = $this->beginWidget("bootstrap.widgets.TbActiveForm", array(
+                    'id' => 'form-enviarGift',
+                    'type' => 'horizontal',
+                    'clientOptions' => array(
+                        'validateOnSubmit' => true,
+                    )
+                )); ?>
+		
+		<fieldset>
+			<legend>Enviar Gift Card</legend>
+			<div>
+				<p class="lead">1. Selecciona una Gift Card</p>
+				<ul class="thumbnails">
+					<li> <img src="http://placehold.it/200x100"> </li>
+					<li> <img src="http://placehold.it/200x100"> </li>
+					<li> <img src="http://placehold.it/200x100"> </li>
+					<li> <img src="http://placehold.it/200x100"> </li>												
+					<li> <img src="http://placehold.it/200x100"> </li>			
+				</ul>
+
+
+			</div>	
+			<div class="row margin_top">
+				<div class="span6">	
+					<p class="lead">2. Personalízala</p>                                       
+                                        
+                                        
+                                        <?php echo $form->errorSummary($envio); ?>
+                                       
+                                        <?php echo $form->textFieldRow($envio, 'nombre', array(
+                                            'placeholder' => 'Nombre del destinatario'
+                                        )); ?>
+                                        
+                                        <?php echo $form->textFieldRow($envio, 'email', array(
+                                            'placeholder' => 'Email del destinatario')); ?>
+                                        
+                                        <?php echo $form->textAreaRow($envio, 'mensaje', array(
+                                            'placeholder' => 'Escribe un mensaje')); ?>
+                                                                                
+					   
+				</div>	
+				<div class="span5">
+					<img src="http://placehold.it/450x250">
+				</div>
+			</div>
+			<div class="control-group row margin_top">
+				<div class="controls pull-right">                                   
+                                    <button type="submit" name="Enviar" class="btn btn-danger"><i class="icon-envelope icon-white"></i> Enviar</button>                                  
+				</div>
+			</div>			
+		</fieldset>
+		
+                <?php $this->endWidget(); ?>
+
+	</section>
+</div>

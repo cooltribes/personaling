dc9a22272cdb86babf41ae3723d3d6849d186195
Rafael Palma <rpalma@upsidecorp.ch>
2013-11-13 17:36:20 -0430

    Arreglo de los montos en detalle de suaurio

--- protected/views/orden/detalleUsuario.php
+++ protected/views/orden/detalleUsuario.php
@@ -99,33 +99,7 @@ $usuario = User::model()->findByPk($orden->user_id);
         Prendas<br/></td>
       <td><p class="T_xlarge margin_top_xsmall"><?php
 
-    if($orden->estado == 7)
-    {
-        $balance = Balance::model()->findByAttributes(array('user_id'=>$usuario->id,'orden_id'=>$orden->id));
-        if(isset($balance))
-        {
-        $a = $balance->total * -1;
-        echo Yii::app()->numberFormatter->formatDecimal($a);}
-    }
-    else
-    {
-        $balance = Balance::model()->findByAttributes(array('user_id'=>$usuario->id,'orden_id'=>$orden->id, 'tipo'=>0));
-
-        if(isset($balance))
-        {
-            if($balance->total < 0){
-                $a = $balance->total * -1;
-                echo Yii::app()->numberFormatter->formatDecimal($a);
-            }
-            else {
-                echo Yii::app()->numberFormatter->formatDecimal($orden->total);
-            }
-        }
-        else {
-                echo Yii::app()->numberFormatter->formatDecimal($orden->total);
-            }
-
-    }
+   echo Yii::app()->numberFormatter->formatDecimal($orden->getMontoActivo());
        ?></p>
 
         <?php

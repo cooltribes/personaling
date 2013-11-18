f47266f11bcaaea8a27598fb3bc77d80d5557a3f
Carlos Linares <clinares@upsidecorp.ch>
2013-11-18 13:00:54 -0430

    PRimeros cabios para nueva tienda

---
+++ protected/views/tienda/index_new.php
@@ -0,0 +1,493 @@
+<?php 
+	$this->breadcrumbs=array(
+	'Tienda',
+	);
+?>
+
+<!-- BAR ON -->
+<section class="bard_tienda">
+
+	 	<ul class="nav unstyled">
+  			<li class="item">Tienda de prendas:</li>
+  			<li class="itemThumbnails">
+  				<div class="dropdown">
+	  				<a href="#" lass="dropdown-toggle" data-toggle="dropdown">
+	  					<img class="img-categoria" src="/images/categorias/5/img.jpg">	
+	  					<b class="caret caretthumbs"></b>	
+	  				</a>
+					<ul class="dropdown-menu thumbnails row">
+						<li class="span1"> 
+		              		<a href="/tienda/categorias2" class="ctgr" >
+		              			<img src="/images/categorias/29/img.jpg" >
+		              		</a>                	
+		              		<div class="caption">
+		                  		<p>Belleza</p>
+			                </div>
+
+	              		</li>
+						<li class="span1"> 
+		              		<a href="/tienda/categorias2" class="ctgr" >
+		              			<img src="/images/categorias/29/img.jpg" >
+		              		</a>                	
+		              		<div class="caption">
+		                  		<p>Belleza</p>
+			                </div>
+
+	              		</li>
+						<li></li>
+						<li></li>															
+					</ul> 
+				</div>   				
+  			</li>
+  			<li class="itemThumbnails">
+  				<a href="#" lass="dropdown-toggle" data-toggle="dropdown">
+  					<img class="img-categoria" src="/images/categorias/3/img.jpg">	
+  					<b class="caret caretthumbs"></b>	
+  				</a>
+				<ul class="dropdown-menu" >
+					<li></li>
+					<li></li>
+					<li></li>
+					<li></li>															
+				</ul>    				
+  			</li>
+  			<li class="itemThumbnails">
+  				<a href="#" lass="dropdown-toggle" data-toggle="dropdown">
+  					<img class="img-categoria" src="/images/categorias/4/img.jpg">		
+  					<b class="caret caretthumbs"></b>
+  				</a>
+				<ul class="dropdown-menu" >
+					<li></li>
+					<li></li>
+					<li></li>
+					<li></li>															
+				</ul>    				
+  			</li>  	
+  			<li class="itemThumbnails">
+  				<a href="#" lass="dropdown-toggle" data-toggle="dropdown">
+  					Color:
+  					<img src="/images/colores/C_Multicolor.jpg" alt="Ccolor">		
+  					<b class="caret caretthumbs"></b>
+  				</a>
+				<ul class="dropdown-menu" >
+					<li></li>
+					<li></li>
+					<li></li>
+					<li></li>															
+				</ul>  
+  			</li>  					  			
+			<li class="item">
+				<div class="dropdown">
+					<a href="#" class="dropdown-toggle" data-toggle="dropdown" >
+						<div class="dropinput">
+							Filtrar por precio
+							<small>
+								<b class="caret"></b>
+							</small>
+						</div>
+					</a>
+					<ul class="dropdown-menu" >
+						<li><a href="#">100,00 Bs</a></li>
+						<li><a href="#">200,00 Bs</a></li>
+						<li><a href="#">300,00 Bs</a></li>
+						<li><a href="#">400,00 Bs</a></li>															
+					</ul>  
+				</div>	
+			</li>
+
+			<li class="item">
+				<div class="dropdown">
+					<a href="#" class="dropdown-toggle" data-toggle="dropdown">
+						<div class="dropinput">
+							Filtrar por marca
+							<small>
+								<b class="caret"></b>
+							</small>
+						</div>						
+					</a>
+					<ul class="dropdown-menu">	
+						<li><a href="#">Mango</a></li>
+						<li><a href="#">Suiteblanco</a></li>
+						<li><a href="#">Timberland</a></li>																		
+					</ul>  	
+				</div>	
+			</li>			
+<!-- 			<li class="dropdown item">
+				<div class="">
+					<input type="text" class="" placeholder="Buscar"> 	
+				</div>
+			</li>	 -->
+		</ul>	 
+
+</section>
+
+<!-- BAR OFF -->
+<!-- PRODUCTOS ON -->
+
+<div class="margin_top tienda_productos">
+      <div class="row">
+		<div class="items">
+
+ 			<article class="span3">
+ 				<div class="producto">
+					<img class="img_hover" width="270" height="270" src="http://placehold.it/270x270" alt="Imagen"> 
+					<a class="btn btn-block btn-small vista_rapida hidden-phone">Vista Rápida</a>		
+					<header>
+						<h3>
+							<a href="#" title="PLACEHOLD.IT">PLACEHOLD.IT</a>
+						</h3>
+						<a href="#" class="ver_detalle icon_lupa" title="Ver detalle"></a>
+					</header>
+					<span class="precio">Bs. 0.000,00</span>
+					<a href="#"  style="cursor:pointer" title="Me encanta" class="entypo like icon_personaling_big">♡</a>
+				</div>
+			</article>	
+
+ 			<article class="span3">
+ 				<div class="producto">
+					<img class="img_hover" width="270" height="270" src="http://placehold.it/270x270" alt="Imagen"> 
+					<a class="btn btn-block btn-small vista_rapida hidden-phone">Vista Rápida</a>		
+					<header>
+						<h3>
+							<a href="#" title="PLACEHOLD.IT">PLACEHOLD.IT</a>
+						</h3>
+						<a href="#" class="ver_detalle icon_lupa" title="Ver detalle"></a>
+					</header>
+					<span class="precio">Bs. 0.000,00</span>
+					<a href="#"  style="cursor:pointer" title="Me encanta" class="entypo like icon_personaling_big">♡</a>
+				</div>
+			</article>	
+
+ 			<article class="span3">
+ 				<div class="producto">
+					<img class="img_hover" width="270" height="270" src="http://placehold.it/270x270" alt="Imagen"> 
+					<a class="btn btn-block btn-small vista_rapida hidden-phone">Vista Rápida</a>		
+					<header>
+						<h3>
+							<a href="#" title="PLACEHOLD.IT">PLACEHOLD.IT</a>
+						</h3>
+						<a href="#" class="ver_detalle icon_lupa" title="Ver detalle"></a>
+					</header>
+					<span class="precio">Bs. 0.000,00</span>
+					<a href="#"  style="cursor:pointer" title="Me encanta" class="entypo like icon_personaling_big">♡</a>
+				</div>
+			</article>	
+
+ 			<article class="span3">
+ 				<div class="producto">
+					<img class="img_hover" width="270" height="270" src="http://placehold.it/270x270" alt="Imagen"> 
+					<a class="btn btn-block btn-small vista_rapida hidden-phone">Vista Rápida</a>		
+					<header>
+						<h3>
+							<a href="#" title="PLACEHOLD.IT">PLACEHOLD.IT</a>
+						</h3>
+						<a href="#" class="ver_detalle icon_lupa" title="Ver detalle"></a>
+					</header>
+					<span class="precio">Bs. 0.000,00</span>
+					<a href="#"  style="cursor:pointer" title="Me encanta" class="entypo like icon_personaling_big">♡</a>
+				</div>
+			</article>		
+
+ 			<article class="span3">
+ 				<div class="producto">
+					<img class="img_hover" width="270" height="270" src="http://placehold.it/270x270" alt="Imagen"> 
+					<a class="btn btn-block btn-small vista_rapida hidden-phone">Vista Rápida</a>		
+					<header>
+						<h3>
+							<a href="#" title="PLACEHOLD.IT">PLACEHOLD.IT</a>
+						</h3>
+						<a href="#" class="ver_detalle icon_lupa" title="Ver detalle"></a>
+					</header>
+					<span class="precio">Bs. 0.000,00</span>
+					<a href="#"  style="cursor:pointer" title="Me encanta" class="entypo like icon_personaling_big">♡</a>
+				</div>
+			</article>	
+
+ 			<article class="span3">
+ 				<div class="producto">
+					<img class="img_hover" width="270" height="270" src="http://placehold.it/270x270" alt="Imagen"> 
+					<a class="btn btn-block btn-small vista_rapida hidden-phone">Vista Rápida</a>		
+					<header>
+						<h3>
+							<a href="#" title="PLACEHOLD.IT">PLACEHOLD.IT</a>
+						</h3>
+						<a href="#" class="ver_detalle icon_lupa" title="Ver detalle"></a>
+					</header>
+					<span class="precio">Bs. 0.000,00</span>
+					<a href="#"  style="cursor:pointer" title="Me encanta" class="entypo like icon_personaling_big">♡</a>
+				</div>
+			</article>	
+
+ 			<article class="span3">
+ 				<div class="producto">
+					<img class="img_hover" width="270" height="270" src="http://placehold.it/270x270" alt="Imagen"> 
+					<a class="btn btn-block btn-small vista_rapida hidden-phone">Vista Rápida</a>		
+					<header>
+						<h3>
+							<a href="#" title="PLACEHOLD.IT">PLACEHOLD.IT</a>
+						</h3>
+						<a href="#" class="ver_detalle icon_lupa" title="Ver detalle"></a>
+					</header>
+					<span class="precio">Bs. 0.000,00</span>
+					<a href="#"  style="cursor:pointer" title="Me encanta" class="entypo like icon_personaling_big">♡</a>
+				</div>
+			</article>	
+
+ 			<article class="span3">
+ 				<div class="producto">
+					<img class="img_hover" width="270" height="270" src="http://placehold.it/270x270" alt="Imagen"> 
+					<a class="btn btn-block btn-small vista_rapida hidden-phone">Vista Rápida</a>		
+					<header>
+						<h3>
+							<a href="#" title="PLACEHOLD.IT">PLACEHOLD.IT</a>
+						</h3>
+						<a href="#" class="ver_detalle icon_lupa" title="Ver detalle"></a>
+					</header>
+					<span class="precio">Bs. 0.000,00</span>
+					<a href="#"  style="cursor:pointer" title="Me encanta" class="entypo like icon_personaling_big">♡</a>
+				</div>
+			</article>		
+
+ 			<article class="span3">
+ 				<div class="producto">
+					<img class="img_hover" width="270" height="270" src="http://placehold.it/270x270" alt="Imagen"> 
+					<a class="btn btn-block btn-small vista_rapida hidden-phone">Vista Rápida</a>		
+					<header>
+						<h3>
+							<a href="#" title="PLACEHOLD.IT">PLACEHOLD.IT</a>
+						</h3>
+						<a href="#" class="ver_detalle icon_lupa" title="Ver detalle"></a>
+					</header>
+					<span class="precio">Bs. 0.000,00</span>
+					<a href="#"  style="cursor:pointer" title="Me encanta" class="entypo like icon_personaling_big">♡</a>
+				</div>
+			</article>	
+
+ 			<article class="span3">
+ 				<div class="producto">
+					<img class="img_hover" width="270" height="270" src="http://placehold.it/270x270" alt="Imagen"> 
+					<a class="btn btn-block btn-small vista_rapida hidden-phone">Vista Rápida</a>		
+					<header>
+						<h3>
+							<a href="#" title="PLACEHOLD.IT">PLACEHOLD.IT</a>
+						</h3>
+						<a href="#" class="ver_detalle icon_lupa" title="Ver detalle"></a>
+					</header>
+					<span class="precio">Bs. 0.000,00</span>
+					<a href="#"  style="cursor:pointer" title="Me encanta" class="entypo like icon_personaling_big">♡</a>
+				</div>
+			</article>	
+
+ 			<article class="span3">
+ 				<div class="producto">
+					<img class="img_hover" width="270" height="270" src="http://placehold.it/270x270" alt="Imagen"> 
+					<a class="btn btn-block btn-small vista_rapida hidden-phone">Vista Rápida</a>		
+					<header>
+						<h3>
+							<a href="#" title="PLACEHOLD.IT">PLACEHOLD.IT</a>
+						</h3>
+						<a href="#" class="ver_detalle icon_lupa" title="Ver detalle"></a>
+					</header>
+					<span class="precio">Bs. 0.000,00</span>
+					<a href="#"  style="cursor:pointer" title="Me encanta" class="entypo like icon_personaling_big">♡</a>
+				</div>
+			</article>	
+
+ 			<article class="span3">
+ 				<div class="producto">
+					<img class="img_hover" width="270" height="270" src="http://placehold.it/270x270" alt="Imagen"> 
+					<a class="btn btn-block btn-small vista_rapida hidden-phone">Vista Rápida</a>		
+					<header>
+						<h3>
+							<a href="#" title="PLACEHOLD.IT">PLACEHOLD.IT</a>
+						</h3>
+						<a href="#" class="ver_detalle icon_lupa" title="Ver detalle"></a>
+					</header>
+					<span class="precio">Bs. 0.000,00</span>
+					<a href="#"  style="cursor:pointer" title="Me encanta" class="entypo like icon_personaling_big">♡</a>
+				</div>
+			</article>		
+
+ 			<article class="span3">
+ 				<div class="producto">
+					<img class="img_hover" width="270" height="270" src="http://placehold.it/270x270" alt="Imagen"> 
+					<a class="btn btn-block btn-small vista_rapida hidden-phone">Vista Rápida</a>		
+					<header>
+						<h3>
+							<a href="#" title="PLACEHOLD.IT">PLACEHOLD.IT</a>
+						</h3>
+						<a href="#" class="ver_detalle icon_lupa" title="Ver detalle"></a>
+					</header>
+					<span class="precio">Bs. 0.000,00</span>
+					<a href="#"  style="cursor:pointer" title="Me encanta" class="entypo like icon_personaling_big">♡</a>
+				</div>
+			</article>	
+
+ 			<article class="span3">
+ 				<div class="producto">
+					<img class="img_hover" width="270" height="270" src="http://placehold.it/270x270" alt="Imagen"> 
+					<a class="btn btn-block btn-small vista_rapida hidden-phone">Vista Rápida</a>		
+					<header>
+						<h3>
+							<a href="#" title="PLACEHOLD.IT">PLACEHOLD.IT</a>
+						</h3>
+						<a href="#" class="ver_detalle icon_lupa" title="Ver detalle"></a>
+					</header>
+					<span class="precio">Bs. 0.000,00</span>
+					<a href="#"  style="cursor:pointer" title="Me encanta" class="entypo like icon_personaling_big">♡</a>
+				</div>
+			</article>	
+
+ 			<article class="span3">
+ 				<div class="producto">
+					<img class="img_hover" width="270" height="270" src="http://placehold.it/270x270" alt="Imagen"> 
+					<a class="btn btn-block btn-small vista_rapida hidden-phone">Vista Rápida</a>		
+					<header>
+						<h3>
+							<a href="#" title="PLACEHOLD.IT">PLACEHOLD.IT</a>
+						</h3>
+						<a href="#" class="ver_detalle icon_lupa" title="Ver detalle"></a>
+					</header>
+					<span class="precio">Bs. 0.000,00</span>
+					<a href="#"  style="cursor:pointer" title="Me encanta" class="entypo like icon_personaling_big">♡</a>
+				</div>
+			</article>	
+
+ 			<article class="span3">
+ 				<div class="producto">
+					<img class="img_hover" width="270" height="270" src="http://placehold.it/270x270" alt="Imagen"> 
+					<a class="btn btn-block btn-small vista_rapida hidden-phone">Vista Rápida</a>		
+					<header>
+						<h3>
+							<a href="#" title="PLACEHOLD.IT">PLACEHOLD.IT</a>
+						</h3>
+						<a href="#" class="ver_detalle icon_lupa" title="Ver detalle"></a>
+					</header>
+					<span class="precio">Bs. 0.000,00</span>
+					<a href="#"  style="cursor:pointer" title="Me encanta" class="entypo like icon_personaling_big">♡</a>
+				</div>
+			</article>		
+
+ 			<article class="span3">
+ 				<div class="producto">
+					<img class="img_hover" width="270" height="270" src="http://placehold.it/270x270" alt="Imagen"> 
+					<a class="btn btn-block btn-small vista_rapida hidden-phone">Vista Rápida</a>		
+					<header>
+						<h3>
+							<a href="#" title="PLACEHOLD.IT">PLACEHOLD.IT</a>
+						</h3>
+						<a href="#" class="ver_detalle icon_lupa" title="Ver detalle"></a>
+					</header>
+					<span class="precio">Bs. 0.000,00</span>
+					<a href="#"  style="cursor:pointer" title="Me encanta" class="entypo like icon_personaling_big">♡</a>
+				</div>
+			</article>	
+
+ 			<article class="span3">
+ 				<div class="producto">
+					<img class="img_hover" width="270" height="270" src="http://placehold.it/270x270" alt="Imagen"> 
+					<a class="btn btn-block btn-small vista_rapida hidden-phone">Vista Rápida</a>		
+					<header>
+						<h3>
+							<a href="#" title="PLACEHOLD.IT">PLACEHOLD.IT</a>
+						</h3>
+						<a href="#" class="ver_detalle icon_lupa" title="Ver detalle"></a>
+					</header>
+					<span class="precio">Bs. 0.000,00</span>
+					<a href="#"  style="cursor:pointer" title="Me encanta" class="entypo like icon_personaling_big">♡</a>
+				</div>
+			</article>	
+
+ 			<article class="span3">
+ 				<div class="producto">
+					<img class="img_hover" width="270" height="270" src="http://placehold.it/270x270" alt="Imagen"> 
+					<a class="btn btn-block btn-small vista_rapida hidden-phone">Vista Rápida</a>		
+					<header>
+						<h3>
+							<a href="#" title="PLACEHOLD.IT">PLACEHOLD.IT</a>
+						</h3>
+						<a href="#" class="ver_detalle icon_lupa" title="Ver detalle"></a>
+					</header>
+					<span class="precio">Bs. 0.000,00</span>
+					<a href="#"  style="cursor:pointer" title="Me encanta" class="entypo like icon_personaling_big">♡</a>
+				</div>
+			</article>	
+
+ 			<article class="span3">
+ 				<div class="producto">
+					<img class="img_hover" width="270" height="270" src="http://placehold.it/270x270" alt="Imagen"> 
+					<a class="btn btn-block btn-small vista_rapida hidden-phone">Vista Rápida</a>		
+					<header>
+						<h3>
+							<a href="#" title="PLACEHOLD.IT">PLACEHOLD.IT</a>
+						</h3>
+						<a href="#" class="ver_detalle icon_lupa" title="Ver detalle"></a>
+					</header>
+					<span class="precio">Bs. 0.000,00</span>
+					<a href="#"  style="cursor:pointer" title="Me encanta" class="entypo like icon_personaling_big">♡</a>
+				</div>
+			</article>		
+
+ 			<article class="span3">
+ 				<div class="producto">
+					<img class="img_hover" width="270" height="270" src="http://placehold.it/270x270" alt="Imagen"> 
+					<a class="btn btn-block btn-small vista_rapida hidden-phone">Vista Rápida</a>		
+					<header>
+						<h3>
+							<a href="#" title="PLACEHOLD.IT">PLACEHOLD.IT</a>
+						</h3>
+						<a href="#" class="ver_detalle icon_lupa" title="Ver detalle"></a>
+					</header>
+					<span class="precio">Bs. 0.000,00</span>
+					<a href="#"  style="cursor:pointer" title="Me encanta" class="entypo like icon_personaling_big">♡</a>
+				</div>
+			</article>	
+
+ 			<article class="span3">
+ 				<div class="producto">
+					<img class="img_hover" width="270" height="270" src="http://placehold.it/270x270" alt="Imagen"> 
+					<a class="btn btn-block btn-small vista_rapida hidden-phone">Vista Rápida</a>		
+					<header>
+						<h3>
+							<a href="#" title="PLACEHOLD.IT">PLACEHOLD.IT</a>
+						</h3>
+						<a href="#" class="ver_detalle icon_lupa" title="Ver detalle"></a>
+					</header>
+					<span class="precio">Bs. 0.000,00</span>
+					<a href="#"  style="cursor:pointer" title="Me encanta" class="entypo like icon_personaling_big">♡</a>
+				</div>
+			</article>	
+
+ 			<article class="span3">
+ 				<div class="producto">
+					<img class="img_hover" width="270" height="270" src="http://placehold.it/270x270" alt="Imagen"> 
+					<a class="btn btn-block btn-small vista_rapida hidden-phone">Vista Rápida</a>		
+					<header>
+						<h3>
+							<a href="#" title="PLACEHOLD.IT">PLACEHOLD.IT</a>
+						</h3>
+						<a href="#" class="ver_detalle icon_lupa" title="Ver detalle"></a>
+					</header>
+					<span class="precio">Bs. 0.000,00</span>
+					<a href="#"  style="cursor:pointer" title="Me encanta" class="entypo like icon_personaling_big">♡</a>
+				</div>
+			</article>	
+
+ 			<article class="span3">
+ 				<div class="producto">
+					<img class="img_hover" width="270" height="270" src="http://placehold.it/270x270" alt="Imagen"> 
+					<a class="btn btn-block btn-small vista_rapida hidden-phone">Vista Rápida</a>		
+					<header>
+						<h3>
+							<a href="#" title="PLACEHOLD.IT">PLACEHOLD.IT</a>
+						</h3>
+						<a href="#" class="ver_detalle icon_lupa" title="Ver detalle"></a>
+					</header>
+					<span class="precio">Bs. 0.000,00</span>
+					<a href="#"  style="cursor:pointer" title="Me encanta" class="entypo like icon_personaling_big">♡</a>
+				</div>
+			</article>																						
+		</div>
+    </div>
+</div>
+<!-- PRODUCTOS OFF -->

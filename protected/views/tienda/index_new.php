5fc1843410cdf1095387949a3d73cf609ff27478
Carlos Linares <clinares@upsidecorp.ch>
2013-11-19 12:52:44 -0430

    Adelantos para nuevo catalogo de tienda

--- protected/views/tienda/index_new.php
+++ protected/views/tienda/index_new.php
@@ -9,72 +9,343 @@
 
 	 	<ul class="nav unstyled">
   			<li class="item">Tienda de prendas:</li>
-  			<li class="itemThumbnails">
+  			<li class="itemThumbnails tienda_iconos">
   				<div class="dropdown">
 	  				<a href="#" lass="dropdown-toggle" data-toggle="dropdown">
 	  					<img class="img-categoria" src="/images/categorias/5/img.jpg">	
 	  					<b class="caret caretthumbs"></b>	
 	  				</a>
-					<ul class="dropdown-menu thumbnails row">
-						<li class="span1"> 
-		              		<a href="/tienda/categorias2" class="ctgr" >
-		              			<img src="/images/categorias/29/img.jpg" >
+					<ul class="dropdown-menu thumbnails ">
+						<li class=""> 
+		              		<a href="#" >
+		              			<img src="/images/categorias/29/img.jpg" width="60">
 		              		</a>                	
 		              		<div class="caption">
 		                  		<p>Belleza</p>
 			                </div>
-
 	              		</li>
-						<li class="span1"> 
-		              		<a href="/tienda/categorias2" class="ctgr" >
-		              			<img src="/images/categorias/29/img.jpg" >
-		              		</a>                	
-		              		<div class="caption">
-		                  		<p>Belleza</p>
-			                </div>
-
-	              		</li>
-						<li></li>
-						<li></li>															
+						<li class=""> 
+              				<a href="#">
+              					<img src="/images/categorias/104/img.jpg" width="60">
+              				</a>                	
+              				<div class="caption">
+                  				<p>Bisutería</p>
+	                		</div>
+              			</li>
+						<li class=""> 
+              				<a href="#">
+              					<img src="/images/categorias/23/img.jpg" width="60">
+              				</a>                	
+              				<div class="caption">
+                  				<p>Bolsos y Monederos</p>
+	                		</div>
+              			</li>
+						<li class=""> 
+              				<a href="#">
+              					<img src="/images/categorias/24/img.jpg" width="60">
+              				</a>                	
+              				<div class="caption">
+                  				<p>Lentes</p>
+	                		</div>
+              			</li>
+						<li class=""> 
+              				<a href="#">
+              					<img src="/images/categorias/105/img.jpg" width="60">
+              				</a>                	
+              				<div class="caption">
+                  				<p>Otros accesorios</p>
+	                		</div>
+              			</li>	
+						<li class=""> 
+              				<a href="#">
+              					<img src="/images/categorias/27/img.jpg" width="60">
+              				</a>                	
+              				<div class="caption">
+                  				<p>Sombreros</p>
+	                		</div>
+              			</li>              			              																
 					</ul> 
 				</div>   				
   			</li>
-  			<li class="itemThumbnails">
-  				<a href="#" lass="dropdown-toggle" data-toggle="dropdown">
-  					<img class="img-categoria" src="/images/categorias/3/img.jpg">	
-  					<b class="caret caretthumbs"></b>	
-  				</a>
-				<ul class="dropdown-menu" >
-					<li></li>
-					<li></li>
-					<li></li>
-					<li></li>															
-				</ul>    				
+  			<li class="itemThumbnails tienda_iconos">
+  				<div class="dropdown">
+	  				<a href="#" lass="dropdown-toggle" data-toggle="dropdown">
+	  					<img class="img-categoria" src="/images/categorias/3/img.jpg">	
+	  					<b class="caret caretthumbs"></b>	
+	  				</a>
+					<ul class="dropdown-menu thumbnails ">
+						<li class=""> 
+		              		<a href="#" >
+		              			<img src="/images/categorias/14/img.jpg" width="60">
+		              		</a>                	
+		              		<div class="caption">
+		                  		<p>Abrigos</p>
+			                </div>
+	              		</li>
+						<li class=""> 
+	          				<a href="#">
+	          					<img src="/images/categorias/60/img.jpg" width="60">
+	          				</a>                	
+	          				<div class="caption">
+	              				<p>Blazers</p>
+	                		</div>
+	          			</li>
+						<li class=""> 
+	          				<a href="#">
+	          					<img src="/images/categorias/62/img.jpg" width="60">
+	          				</a>                	
+	          				<div class="caption">
+	              				<p>Blusas</p>
+	                		</div>
+	          			</li>
+						<li class=""> 
+	          				<a href="#">
+	          					<img src="/images/categorias/67/img.jpg" width="60">
+	          				</a>                	
+	          				<div class="caption">
+	              				<p>Bragas</p>
+	                		</div>
+	          			</li>
+						<li class=""> 
+	          				<a href="#">
+	          					<img src="/images/categorias/7/img.jpg" width="60">
+	          				</a>                	
+	          				<div class="caption">
+	              				<p>Camisas</p>
+	                		</div>
+	          			</li>	
+						<li class=""> 
+	          				<a href="#">
+	          					<img src="/images/categorias/8/img.jpg" width="60">
+	          				</a>                	
+	          				<div class="caption">
+	              				<p>Camisetas</p>
+	                		</div>
+	          			</li>        
+						<li class=""> 
+	          				<a href="#">
+	          					<img src="/images/categorias/59/img.jpg" width="60">
+	          				</a>                	
+	          				<div class="caption">
+	              				<p>Chaquetas</p>
+	                		</div>
+	          			</li>     
+						<li class=""> 
+	          				<a href="#">
+	          					<img src="/images/categorias/10/img.jpg" width="60">
+	          				</a>                	
+	          				<div class="caption">
+	              				<p>Faldas</p>
+	                		</div>
+	          			</li>       
+						<li class=""> 
+	          				<a href="#">
+	          					<img src="/images/categorias/9/img.jpg" width="60">
+	          				</a>                	
+	          				<div class="caption">
+	              				<p>Franelas</p>
+	                		</div>
+	          			</li>                 			         			            			      			              																
+					</ul>   
+				</div>  				
   			</li>
-  			<li class="itemThumbnails">
-  				<a href="#" lass="dropdown-toggle" data-toggle="dropdown">
-  					<img class="img-categoria" src="/images/categorias/4/img.jpg">		
-  					<b class="caret caretthumbs"></b>
-  				</a>
-				<ul class="dropdown-menu" >
-					<li></li>
-					<li></li>
-					<li></li>
-					<li></li>															
-				</ul>    				
+  			<li class="itemThumbnails tienda_iconos">
+  				<div class="dropdown">
+	  				<a href="#" lass="dropdown-toggle" data-toggle="dropdown">
+	  					<img class="img-categoria" src="/images/categorias/4/img.jpg">		
+	  					<b class="caret caretthumbs"></b>
+	  				</a>
+					<ul class="dropdown-menu thumbnails ">
+						<li class=""> 
+		              		<a href="#" >
+		              			<img src="/images/categorias/19/img.jpg" width="60">
+		              		</a>                	
+		              		<div class="caption">
+		                  		<p>Bailarinas</p>
+			                </div>
+	              		</li>
+						<li class=""> 
+	          				<a href="#">
+	          					<img src="/images/categorias/20/img.jpg" width="60">
+	          				</a>                	
+	          				<div class="caption">
+	              				<p>Botas</p>
+	                		</div>
+	          			</li>
+						<li class=""> 
+	          				<a href="#">
+	          					<img src="/images/categorias/21/img.jpg" width="60">
+	          				</a>                	
+	          				<div class="caption">
+	              				<p>Deporte</p>
+	                		</div>
+	          			</li>
+						<li class=""> 
+	          				<a href="#">
+	          					<img src="/images/categorias/69/img.jpg" width="60">
+	          				</a>                	
+	          				<div class="caption">
+	              				<p>Mocasines</p>
+	                		</div>
+	          			</li>
+						<li class=""> 
+	          				<a href="#">
+	          					<img src="/images/categorias/22/img.jpg" width="60">
+	          				</a>                	
+	          				<div class="caption">
+	              				<p>Plataformas</p>
+	                		</div>
+	          			</li>	
+						<li class=""> 
+	          				<a href="#">
+	          					<img src="/images/categorias/68/img.jpg" width="60">
+	          				</a>                	
+	          				<div class="caption">
+	              				<p>Pumps</p>
+	                		</div>
+	          			</li>        
+						<li class=""> 
+	          				<a href="#">
+	          					<img src="/images/categorias/17/img.jpg" width="60">
+	          				</a>                	
+	          				<div class="caption">
+	              				<p>Sandalias</p>
+	                		</div>
+	          			</li>           
+						<li class=""> 
+	          				<a href="#">
+	          					<img src="/images/categorias/18/img.jpg" width="60">
+	          				</a>                	
+	          				<div class="caption">
+	              				<p>Tacones</p>
+	                		</div>
+	          			</li>                 			         			            			      			              																
+					</ul>   
+				</div>    				
   			</li>  	
-  			<li class="itemThumbnails">
-  				<a href="#" lass="dropdown-toggle" data-toggle="dropdown">
-  					Color:
-  					<img src="/images/colores/C_Multicolor.jpg" alt="Ccolor">		
-  					<b class="caret caretthumbs"></b>
-  				</a>
-				<ul class="dropdown-menu" >
-					<li></li>
-					<li></li>
-					<li></li>
-					<li></li>															
-				</ul>  
+  			<li class="itemThumbnails tienda_iconos">
+  				<div class="dropdown">
+	  				<a href="#" lass="dropdown-toggle" data-toggle="dropdown">
+	  					Color:
+	  					<img src="/images/colores/C_Multicolor.jpg" alt="Ccolor" width="20">		
+	  					<b class="caret caretthumbs"></b>
+	  				</a>
+					<ul class="dropdown-menu dropdown-colors thumbnails ">
+						<li class="colors"> 
+		              		<a href="#" >
+		              			<img  src="/images/colores/C_Beige.jpg" width="44">
+		              		</a>                	
+		              		<div class="caption"</div>
+	              		</li>
+						<li class="colors"> 
+	          				<a href="#">
+	          					<img  src="/images/colores/C_Azul.jpg" width="44">
+	          				</a>                	
+	          			
+	          			</li>
+						<li class="colors"> 
+	          				<a href="#">
+	          					<img  src="/images/colores/C_Verde.jpg" width="44">
+	          				</a>                	
+	          			
+	          			</li>
+						<li class="colors"> 
+	          				<a href="#">
+	          					<img  src="/images/colores/C_Naranja.jpg" width="44">
+	          				</a>                	
+	          			
+	          			</li>
+						<li class="colors"> 
+	          				<a href="#">
+	          					<img  src="/images/colores/C_Morado.jpg" width="44">
+	          				</a>                	
+	          			
+	          			</li>	
+						<li class="colors"> 
+	          				<a href="#">
+	          					<img  src="/images/colores/C_Plateado.jpg" width="44">
+	          				</a>                	
+	          			
+	          			</li>        
+						<li class="colors"> 
+	          				<a href="#">
+	          					<img  src="/images/colores/C_Amarillo.jpg" width="44">
+	          				</a>                	
+	          			
+	          			</li>           
+						<li class="colors"> 
+	          				<a href="#">
+	          					<img  src="/images/colores/C_Negro.jpg" width="44">
+	          				</a>                	
+	          			
+	          			</li> 
+						<li class="colors"> 
+	          				<a href="#">
+	          					<img  src="/images/colores/C_Marron.jpg" width="44">
+	          				</a>                	
+	          			
+	          			</li>  
+						<li class="colors"> 
+	          				<a href="#">
+	          					<img  src="/images/colores/C_Dorado.jpg" width="44">
+	          				</a>                	
+	          			
+	          			</li>  	        
+						<li class="colors"> 
+	          				<a href="#">
+	          					<img  src="/images/colores/C_Gris.jpg" width="44">
+	          				</a>                	
+	          			
+	          			</li>  	   
+						<li class="colors"> 
+	          				<a href="#">
+	          					<img  src="/images/colores/C_Rosado.jpg" width="44">
+	          				</a>                	
+	          			
+	          			</li>
+						<li class="colors"> 
+	          				<a href="#">
+	          					<img  src="/images/colores/C_Rojo.jpg" width="44">
+	          				</a>                	
+	          			
+	          			</li>
+						<li class="colors"> 
+	          				<a href="#">
+	          					<img  src="/images/colores/C_Blanco.jpg" width="44">
+	          				</a>                	    			
+	          			</li>  
+						<li class="colors"> 
+	          				<a href="#">
+	          					<img  src="/images/colores/C_Multicolor.jpg" width="44">
+	          				</a>                	    			
+	          			</li> 
+						<li class="colors"> 
+	          				<a href="#">
+	          					<img  src="/images/colores/cuadrofucsia.jpg" width="44">
+	          				</a>                	    			
+	          			</li> 	
+						<li class="colors"> 
+	          				<a href="#">
+	          					<img  src="/images/colores/C_Coral.jpg" width="44">
+	          				</a>                	    			
+	          			</li> 	
+						<li class="colors"> 
+	          				<a href="#">
+	          					<img  src="/images/colores/large.jpg" width="44">
+	          				</a>                	    			
+	          			</li> 	
+						<li class="colors"> 
+	          				<a href="#">
+	          					<img  src="/images/colores/C_Turquesa.jpg" width="44">
+	          				</a>                	    			
+	          			</li> 	
+						<li class="colors"> 
+	          				<a href="#">
+	          					<img  src="/images/colores/esmeralda.jpg" width="44">
+	          				</a>                	    			
+	          			</li> 	          			          			          			          			          				          				   	          				          				          				          			  				          			                			         			            			      			              																
+					</ul>  
+				</div>
   			</li>  					  			
 			<li class="item">
 				<div class="dropdown">

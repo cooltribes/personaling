1e0b421730778f925f8c00a23887df1000b30e34
Rafael Palma <rpalma@upsidecorp.ch>
2013-12-17 19:27:47 -0430

    Validar que no se puedan comprar productos que no esten en existencia

--- protected/controllers/BolsaController.php
+++ protected/controllers/BolsaController.php
@@ -977,10 +977,13 @@ class BolsaController extends Controller
 			if ($codigo_randon == $_POST['codigo_randon'])
 				Yii::app()->end();
 			Yii::app()->getSession()->add('codigo_randon',$codigo_randon);	
+		 	
 		 	$respCard = "";
 		 	$usuario = Yii::app()->user->id; 
 			$user = User::model()->findByPk($usuario);
 			$bolsa = Bolsa::model()->findByAttributes(array('user_id'=>$usuario));
+			if (!$bolsa->checkInventario())
+				$this->redirect($this->createAbsoluteUrl('bolsa/index',array('mensaje'=>"Hola"),'http'));
 			$tipoPago = Yii::app()->getSession()->get('tipoPago');		
 			switch ($tipoPago) {
 			    case 1: // TRANSFERENCIA

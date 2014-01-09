91c381a5aac6e7ddcf0072cef3965b4ec7a5e2ff
Carlos Linares <clinares@upsidecorp.ch>
2014-01-09 12:52:44 -0430

    Agredo a tienda de looks 100%chic

--- protected/views/tienda/_look.php
+++ protected/views/tienda/_look.php
@@ -8,15 +8,15 @@
   <div class="row" id="looks">
   	
 	<?php foreach($looks as $look): 
-		$chic="";
-		if($look->has_100chic){
-			$chic="has_100chic";
-		}
+
 		?>
 		
 		<?php  //echo $this->renderPartial('_look',array('look'=>$look),true,true); ?>
-<div class="span4 look <?php echo $chic?>">
+<div class="span4 look">
       <article > 
+      	<?php if($look->has_100chic){ ?>
+			<div class="has_100chic"></div>
+      	<?php }?>
       	<?php if ($pages->currentPage > 0){ ?>
       	<?php $image = CHtml::image(Yii::app()->createUrl('look/getImage',array('id'=>$look->id,'w'=>'368','h'=>'368')), "Look", array("id" => "imglook".$look->id,"width" => "368", "height" => "368", 'class'=>'imglook')); ?>
       	<?php }else{ ?>

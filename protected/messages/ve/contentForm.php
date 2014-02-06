54722d9da2136e2ba4b674b1d32dbc80cae176fe
Carlos Linares <clinares@upsidecorp.ch>
2014-02-06 18:28:08 -0430

    Agregado nuevas palabras

--- protected/messages/ve/contentForm.php
+++ protected/messages/ve/contentForm.php
@@ -3,7 +3,114 @@
 return array (
 
      'PRONUNCIACION' => 'Venezolana',
-     'currSym'=>'Bs'
+     'currSym'=>'Bs',
+
+
+     'Amount' => 'Monto',
+     'Amount returned' => 'Monto devuelto',
+     'Awaiting confimation' => 'Espera confirmación',
+     'Awaiting confirmation deposit'=> 'Depósito en espera de confirmación',
+     'Awaiting payment' => 'En espera de pago',
+
+
+     'Balance' => 'Saldo',
+     'Brand' => 'Marca',
+
+
+     'Credit Card' => 'Tarjeta de Credito',
+     'Color' => 'Color',
+     'Courier delivery' => 'Transportista',
+     'Current state' => 'Estado Actual',
+
+     
+     'Date' => 'Fecha',
+     'Date estimated delivery' => 'Fecha estimada de entrega',
+     'Deposit or Transference' => 'Depósito o Transferencia',
+     'Discount' => 'Descuento',
+     'Documents' => 'Documentos',
+
+
+     'Email' => 'Email',
+
+     
+     'ID Transaction' => 'ID de Transaccion',
+     'Individual items' => 'Prendas individuales',
+     'Input' => 'Entrada',
+     'Items' => 'Prendas',
+
+
+     'MercadoPago' => 'MercadoPago',
+     'Message history' => 'Historial de Mensajes',
+     'Messages' => 'Mensajes',
+     'Missing' => 'que faltan',
+
+     
+     'None of the items returned' => 'No se ha devuelto ningún producto de esta orden',
+     'No messages have been posted' => 'No se han enviado mensajes'
+     'Nro. looks' => 'No. de looks',
+     'Nro items' => 'No. de Prendas',
+     'Name of items' => 'Nombre de la prenda',
+     'Name' => 'Nombre',
+
+
+     'Order' => 'Pedido',
+     'Order Cancelled' => 'Orden Cancelada',
+     'Order Date' => 'Fecha de pedido',
+     'Order Sent' => 'Pedido enviado',
+     'Order Summary' => 'Resumen del pedido',
+     'Output' => 'Salida',
+     
+     'Paid' => 'Ya pagados',
+     'Parcial Returned' => 'Devolución Parcial'.
+     'Payment confirmet' => 'Pago confirmado',
+     'Payment declined' => 'Pago Rechazado',
+     'Payment report' => 'Informe de pago',
+     'Payment method' => 'Métido de Pago',
+     'Pending payment' => 'Pendientes por pagar',
+     'Pending confirmation' => 'Pendientes por confirmar',
+     'Phone' => 'Teléfono',
+     'Print order' => 'Imprimir Pedido',
+     'Price' => 'Precio',
+     'Products' => 'Productos',
+     'Progress order' => 'Progreso del pedido',
+     
+
+     'Quantity' => 'Cant.',
+     
+     
+     'Reason' => 'Motivo',
+     'Reference' => 'Referencia',
+     'Retuned items' => 'Productos devueltos',
+     'Returned' => 'Devuelto',
+
+
+     'Shipping address' => 'Dirección de envío',
+     'Shipping cost' => 'Costo de envío',
+     'State' => 'Estado',
+     'Size' => 'Talla',
+     'Subtotal' => 'Subtotal',
+     'Shipping' =>'Envío y transporte',
+     'Sumary' => 'Resumen',
+     'Send Comment' => 'Enviar comentario',
+     'Subject' => 'Asunto',
+
+
+     'Tax' => 'Impuesto',
+     'Total' => 'Total',
+     'Total returned' => 'Total devuelto',
+     'Type' => 'Tipo',
+     'Tracking' => 'Número de seguimiento',
+
+
+     'Underpayment' => 'Pago Insuficiente',
+     'User' => 'Usuario',
+
+
+     'Value for returned shipping' => 'Monto por envío devuelto',
+
+
+     'Weigth' => 'Peso',
+
 
 );
 

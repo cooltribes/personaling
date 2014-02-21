7278a9708fdce9403ac1aec9fbe62309453ced41
Nelson Ramirez <nramirez@stcsolutions.com.ve>
2014-02-21 13:32:14 -0430

    Bug - Correccion de desactivar giftcard

--- protected/views/giftcard/index.php
+++ protected/views/giftcard/index.php
@@ -23,14 +23,94 @@ $this->breadcrumbs = array(
     <!-- FLASH OFF --> 
         <h1>Administrar Gift cards</h1>
     </div>
+    <style>
+        input.input-search{
+            height: 18px;
+        }
+    </style>
     <div class="row">
-        <div class="span2">
-            <a href="create" class="btn btn-success">Crear Gift Card</a>
+  <!--       <div class="span3">
+          <div class="input-append">
+           <form class="no_margin_bottom form-search">
+                <input type="text" name="query" id="query" class="span2 input-search">
+                <a class="btn" id="btn_search_event"><i class="icon-search"></i> Buscar</a>
+            </form>
+            <?php
+            /*Codigo para hacer una búsqueda haciendo click en el boton*/
+           /* Yii::app()->clientScript->registerScript('query1',
+                    "var ajaxUpdateTimeout;
+                    var ajaxRequest;
+                    $('#btn_search_event').click(function(){
+                        $('.crear-filtro').click();
+                        ajaxRequest = $('#query').serialize();
+                        clearTimeout(ajaxUpdateTimeout);
+
+                        ajaxUpdateTimeout = setTimeout(function () {
+                        $.fn.yiiListView.update(
+                        'list-auth-items',
+                        {
+                        type: 'POST',	
+                        url: '" . CController::createUrl('producto/admin') . "',
+                        data: ajaxRequest}
+
+                        )
+                        },
+
+                    300);
+                    return false;
+                    });",CClientScript::POS_READY
+            );
+
+            // Codigo para actualizar el list view cuando presionen ENTER
+
+            Yii::app()->clientScript->registerScript('query',
+                    "var ajaxUpdateTimeout;
+                    var ajaxRequest; 
+
+                    $(document).keypress(function(e) {
+                        if(e.which == 13) {
+                        $('.crear-filtro').click();
+                            ajaxRequest = $('#query').serialize();
+                                    clearTimeout(ajaxUpdateTimeout);
+
+                                    ajaxUpdateTimeout = setTimeout(function () {
+                                            $.fn.yiiListView.update(
+                                            'list-auth-items',
+                                            {
+                                            type: 'POST',	
+                                            url: '" . CController::createUrl('producto/admin') . "',
+                                            data: ajaxRequest}
+
+                                            )
+                                            },
+
+                            300);
+                            return false;
+                        }
+                    });",CClientScript::POS_READY
+            );	
+            
+            */
+            ?>	
+
         </div>
-        <div class="span3">
-            <a href="createMasivo" class="btn btn-success">Crear Gift Card Masivo</a>
+        </div>-->
+    <div class="span3 offset1">   
+        
+        <?php echo CHtml::dropDownList("Filtros", "", Chtml::listData(Filter::model()->findAll('type = 7'),
+                "id_filter", "name"), array('empty' => '-- Filtros Preestablecidos --', 'id' => 'all_filters')) ?>
+
+    </div>
+        <div class="span2"><a href="#" class="btn crear-filtro">Crear nuevo filtro</a></div>
+        <div class="span2 offset2">
+            <a href="create" class="btn btn-success">Crear GiftCard</a>
+        </div>
+        <div class="span2">
+            <a href="createMasivo" class="btn btn-success">Exportación Masiva</a>
         </div>        
     </div>
+    <hr/>
+        <?php $this->renderPartial('_filters'); ?>
     <hr/>
     <style>
         .table th{
@@ -47,9 +127,9 @@ $this->breadcrumbs = array(
             <th rowspan="2" scope="col">Id</th>
             <th rowspan="2" scope="col">Comprador</th>
             <th rowspan="2" scope="col">Estado</th>
-            <th rowspan="2" scope="col">Monto Bs.</th>
+            <th rowspan="2" scope="col">Monto en '.Yii::t('contentForm', 'currSym').'</th>
             <th colspan="2" scope="col">Vigencia</th>
-            <th rowspan="2" scope="col">Fecha de Aplicacion</th>
+            <th rowspan="2" scope="col">Fecha de Aplicación<br>Usuario que la aplicó</th>
             <th rowspan="2" scope="col">Acciones</th>
         </tr>
         <tr>
@@ -68,14 +148,16 @@ $this->breadcrumbs = array(
         'template' => $template,
         'afterAjaxUpdate' => " function(id, data) {						    	
                         $('#todos').click(function() { 
-                inputs = $('table').find('input').filter('[type=checkbox]');
+                            inputs = $('table').find('input').filter('[type=checkbox]');
 
-                                if($(this).attr('checked')){
-                     inputs.attr('checked', true);
-                }else {
-                     inputs.attr('checked', false);
-                } 	
+                                            if($(this).attr('checked')){
+                                 inputs.attr('checked', true);
+                            }else {
+                                 inputs.attr('checked', false);
+                            } 	
                         });
+                        
+                        desactivarGC();
 
                     } ",
         'pager' => array(
@@ -88,6 +170,8 @@ $this->breadcrumbs = array(
     ?>
   
     <hr/>
+    
+    <?php //$this->render ?>
 <!--    <div class="row">
       <div class="span3">
         <select class="span3">
@@ -101,10 +185,42 @@ $this->breadcrumbs = array(
     </div>-->
 
 </div>
+<style>
+    #modalGiftCard .modal-body{
+        min-height: 300px;
+        max-height: 1000px;
+    }
+</style>
+<!------------------- MODAL WINDOW ON ----------------->
+<?php $this->beginWidget("bootstrap.widgets.TbModal", array(
+   'id' => "modalGiftCard", 
+)); ?>
+
+<div class="modal-header">
+    <button type="button" class="close" data-dismiss="modal" aria-hidden="true">&times;</button>
+    <h3>GiftCard</h3>
+</div>
+
+<div class="modal-body">
+    
+</div>
+
+<div class="modal-footer">
+    <a href="" data-dismiss="modal" class="btn btn-danger">Cerrar</a>    
+</div>
+
+<?php $this->endWidget(); ?>
+<!------------------- MODAL WINDOW OFF ----------------->
+
+
 <script type="text/javascript">
+
+/*Desactivar giftcard*/    
+function desactivarGC(){
     
     
-        $("[id^='linkDesactivar']").click(function (e){
+
+$("[id^='linkDesactivar']").click(function (e){
             e.preventDefault();
             //console.log("click");
             var idString = $(this).attr('id');    
@@ -119,7 +235,7 @@ $this->breadcrumbs = array(
                 
                     if(data.status == 'success'){
                         
-                       bootbox.alert("¡La Gift Card se ha desactivado éxito!");
+                       bootbox.alert("¡La Gift Card se ha desactivado con éxito!");
                        
                        ajaxUpdateTimeout = setTimeout(function () {
                        $.fn.yiiListView.update(
@@ -133,8 +249,6 @@ $this->breadcrumbs = array(
                        )
                        },
                        300);
-
-                       
                        
                     }else if(data == 'error'){
                        bootbox.alert("Error desactivando la Gift Card");
@@ -143,6 +257,54 @@ $this->breadcrumbs = array(
             });
             
         });
+}    
+
+/*Ver modal con datos de la giftcard*/
+function ver(id){
+    
+    var urlModal = "<?php echo CController::createUrl("/tienda/modalAjax"); ?>";  
+    $("#modalGiftCard .modal-body").empty();        
+    $.ajax({
+        type: 'POST',
+        url: urlModal,
+        dataType: 'JSON',
+        data: {modal : "giftcard", id : id},
+        success: function(data){
+            
+            $("#modalGiftCard .modal-body").html(data.data);
+            //$("#modalPerfilesOcultos").modal("show");
+
+        }
+    });
+
+}
+
+
+function changeFilter(e){
+   var column = $(this);
+   
+   //si es fecha
+   if(column.val() === 'inicio_vigencia' || column.val() === 'fin_vigencia') //Fechas
+   {
+       dateFilter(column);
+    
+   }else if(column.val() === 'estado') //Estado del usuario, tipo usuario, fuenteRegistro
+   {       
+       
+       listFilter(column, column.val());
+        
+
+   }else if(column.val() === 'comprador' || column.val() === 'beneficiario') 
+   {
+       textFilter(column);       
+        
+   }else //campo normal (numérico)
+   {      
+      valueFilter(column);       
+      
+   }
+    
+}
 
 </script>
 <?php 

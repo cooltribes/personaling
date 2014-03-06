a1ee4ef503165f2e799a03396fc9f380d69f7a6c
unknown <Carlos@.(none)>
2014-03-06 01:16:20 -0430

    Guardar-Mostrar nueva direccion

--- protected/views/bolsa/direcciones.php
+++ protected/views/bolsa/direcciones.php
@@ -6,7 +6,7 @@ if (!Yii::app()->user->isGuest) { // que este logueado
     $nombre = $userObject ? $userObject->profile->first_name." ".$userObject->profile->last_name:
                 "";
 ?>
-
+<script> var error=0;</script>
 <div class="container margin_top">
     <div class="progreso_compra">
     <div class="clearfix margin_bottom">
@@ -41,78 +41,19 @@ if (!Yii::app()->user->isGuest) { // que este logueado
 //     	$usuario = Yii::app()->user->id; 
      	$usuario = $user; 
         $direcciones = Direccion::model()->findAllByAttributes(array('user_id'=>$usuario));
-     
+     	echo CHtml::hiddenField('admin',$admin);
+		echo CHtml::hiddenField('user',$user);
       ?>
 	  <?php if( count( $direcciones ) > 0 ){ ?>
 	  <section class="bg_color3 margin_top  margin_bottom_small padding_small box_1">
-          <fieldset>
+          <fieldset id="anteriores">
             <legend ><?php echo Yii::t('contentForm','Addresses used above'); ?>: </legend>
             <?php
             }
             if(isset($direcciones)){
-	       		foreach($direcciones as $cadauna){
-	       			$ciudad = Ciudad::model()->findByPk($cadauna->ciudad_id);
-					$provincia = Provincia::model()->findByPk($cadauna->provincia_id);
-
-			       $form=$this->beginWidget('bootstrap.widgets.TbActiveForm',array(
-						'id'=>'direccionUsada',
-						'enableAjaxValidation'=>false,
-						'enableClientValidation'=>true,
-						'clientOptions'=>array(
-							'validateOnSubmit'=>true, 
-						),
-						'htmlOptions'=>array('class'=>'form-horizontal  direccion'.$cadauna->id ),
-					));
-						
-		            echo $form->hiddenField($cadauna, 'id', array('value'=>$cadauna->id,'type'=>'hidden'));	 	    
-		            echo CHtml::hiddenField('tipo','direccionVieja');
-		            echo CHtml::hiddenField('admin',$admin);
-		            echo CHtml::hiddenField('user',$user);
-					
-		            echo "
-		            <div class='row'>
-		            
-		              <div class='span2'>
-		                <p><strong>".$cadauna->nombre." ".$cadauna->apellido."</strong><br/>
-		                  <span class='muted small'> ".Yii::t('contentForm','C.I')." ".$cadauna->cedula."</span></p>
-		                <p> <strong>	".Yii::t('contentForm','Phone')."</strong>: ".$cadauna->telefono."</p>
-		              </div>
-		              <div class='span3'>
-		                <p><strong>".Yii::t('contentForm','Address').":</strong> <br/>
-		                  ".$cadauna->dirUno." </br>
-		                  ".$cadauna->dirDos.". 
-		                  ".$ciudad->nombre.", ".$provincia->nombre.". </br>
-		                  ".$cadauna->pais." </p>
-		                      
-		              </div>
-		              <div class='span2 margin_top_medium'>
-		                <p>
-					";
-					$this->widget('bootstrap.widgets.TbButton', array(
-			            'buttonType'=>'submit',
-			            'type'=>'danger',
-			            'size'=>'normal',
-			            'label'=>Yii::t('contentForm','Use this address'),
-			        )); 
-					     	
-					echo"
-		                <br/>
-		                  <a style='cursor: pointer;' onclick='editar(".$cadauna->id.")' title='editar'>".Yii::t('contentForm','Edit')."</a> <br/>
-		                  <a style='cursor: pointer;' onclick='eliminar(".$cadauna->id.")' title='eliminar' data-loading-text='Eliminando...' id='eliminar".$cadauna->id."'>".Yii::t('contentForm','Delete')."</a></p>
-		              </div>
-		            </div>
-		            <div class='border_1'>		            	
-						<label class='checkbox'>
-						  <input class='billingAddress' type='checkbox' value=''>
-						  Utilizar como direccion de facturación.
-						</label>
-		            </div>
-			  		<div class='mensaje".$cadauna->id."' ></div>
-		            <hr/>";
-			  		
-			  		$this->endWidget();
-
-			  	}
+	       		$this->renderPartial('_direcciones', array(
+	       		'direcciones'=>$direcciones) , 
+	       		false);
 	  		}
 			else {
 				echo "<legend>".Yii::t('contentForm','You don\'t have any saved address')."</legend>";					
@@ -132,6 +73,18 @@ if (!Yii::app()->user->isGuest) { // que este logueado
 	'enableClientValidation'=>true,
 	'clientOptions'=>array(
 		'validateOnSubmit'=>true, 
+		'afterValidate'=>"js:function(form, data, hasError) {
+				if(!hasError)
+				{agregar(); }
+				
+				
+ 
+}
+	
+	
+	
+	"
+		
 	),
 	'htmlOptions'=>array('class'=>'form-horizontal'),
 )); 
@@ -243,11 +196,15 @@ if (!Yii::app()->user->isGuest) { // que este logueado
               </div>
             </div>
             <div class="form-actions">
+            	
+            	<input type="button" onclick="agregar()" value="BOTON"/>
+            	
             <?php $this->widget('bootstrap.widgets.TbButton', array(
             'buttonType'=>'submit',
             'type'=>'danger',
             'size'=>'large',
-            'label'=>Yii::t('contentForm','Use this address'),
+            'label'=>'Guardar en mis direcciones',
+            'id'=>'agregar'
         )); 
 		
 		
@@ -286,6 +243,60 @@ else
 ?>
 
 <script>
+	
+	
+	$('#direccion_nueva').submit(function(e) {
+    		e.preventDefault();
+    		
+	 });
+	
+	
+	function agregar(){
+		
+				var nom=$('#Direccion_nombre').val();
+				var ap=$('#Direccion_apellido').val();
+				var ced=$('#Direccion_cedula').val();
+				
+				var d1 =$('#Direccion_dirUno').val();
+				var d2=$('#Direccion_dirDos').val();
+				var tel=$('#Direccion_telefono').val();
+				var prov=$('#Direccion_provincia_id').val();
+				var ciu=$('#Direccion_ciudad_id').val();
+				var pa=$('#Direccion_pais').val();
+				var us=<?php echo $usuario; ?>
+				
+			
+	    		$.ajax({
+				      url: "<?php echo Yii::app()->createUrl('direccion/addDireccion'); ?>",
+				      type: "post",
+				      data: {
+				      	nombre:nom,
+				      	apellido:ap,
+				      	cedula:ced,
+				      	dirUno:d1,
+				      	dirDos:d2,
+				      	telefono:tel,
+				      	provincia_id:prov,
+				      	ciudad_id:ciu,
+				      	pais:pa,
+				      	user_id:us
+				      	
+				      	 },
+				      success: function(data){
+				           $('#anteriores').html(data);
+				      },
+				      error:function(){
+				      	alert("NOTHIN'");
+				      }
+				});
+			
+		
+	}
+	
+	
+	
+	
+	
 	
 	function eliminar(id)
 	{
@@ -344,6 +355,10 @@ else
 			});
 		}
 	});
+	
+	
+	
+	
 	$('.billingAddress').change(function(){
 
 			$('.billingAddress').attr('checked', false);

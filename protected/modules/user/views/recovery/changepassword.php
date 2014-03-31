52cd7783c2c7f74c2c1f5cf30a3fe055dbd8507d
Carlos Linares <clinares@upsidecorp.ch>
2014-03-31 11:38:53 -0430

    Agregando copys en español

--- protected/modules/user/views/recovery/changepassword.php
+++ protected/modules/user/views/recovery/changepassword.php
@@ -11,7 +11,7 @@ $this->breadcrumbs=array(
     <section class="bg_color3 margin_top  margin_bottom_small padding_small box_1">
     <div class="form personaling_form"> <?php echo CHtml::beginForm(); ?>
       
-      <?php echo CHtml::errorSummary($form); ?>
+      <?php echo CHtml::errorSummary($form, "Corrije los siguientes errores:"); ?>
       <div class="control-group"> <?php echo CHtml::activeLabelEx($form,'password'); ?>
         <div class="controls"><?php echo CHtml::activePasswordField($form,'password', array('class'=>'span5')); ?>
           <p class="hel-block muted"> <?php echo UserModule::t("Minimal password length 4 symbols."); ?> </p>

34d70220d95810b99175109d1a64ccd25c831b05
CRuiz <cruiz@upsidecorp.ch>
2014-04-10 16:57:44 -0430

    enter

--- protected/views/tienda/xmltest.php
+++ protected/views/tienda/xmltest.php
@@ -1,4 +1,5 @@
-<?php header("Content-type: text/xml"); header('Content-Disposition: attachment; filename="text.xml"'); ?>
+<?php header("Content-type: text/xml"); 
+header('Content-Disposition: attachment; filename="text.xml"'); ?>
 <?php echo '<?xml version="1.0" encoding="UTF-8"?>'; $content="HOLA";?>
 
 <Response>         

c18755a6a44717674686645fcd10669724d1aaec
Carlos Linares <clinares@upsidecorp.ch>
2014-05-08 16:16:59 -0430

    Cambiando copy de políticas de devoluciones

--- protected/messages/es_es/contentForm.php
+++ protected/messages/es_es/contentForm.php
@@ -500,26 +500,7 @@ return array (
     <p>Si una devolución modifica las condiciones de venta de una promoción (cheques, descuentos, cupones, etc.), se deducirá la parte proporcional de la promoción en función de los artículos devueltos en cada caso.</p>
     <p>Si tienes cualquier duda, puedes ponerte en contacto con nosotros vía mail en <a href="mailto:info@personaling.com">info@personaling.com</p>',
 
-        'Copy Politicas de Devoluciones' => '     
-          <div class="page-header">
-             <h1>Políticas de devoluciones</h1>
-           </div>
-           <p class="lead">A continuación te detallamos las políticas de  devoluciones de <a href="http://www.personaling.com">www.personaling.com</a> la cual rige a partir de tu registro o cuando compras en nuestra  plataforma, por lo que es muy importante que te asegures de leerlas y entenderlas con antelación. Cualquier cambio en su contenido o alcance, sea por requerimientos legislativos o corporativos, serán notificados a nuestros usuarios  mediante sus correos electrónicos. </p>
-           <ol>
-             <li> Solo  se aceptarán devoluciones bajo los siguientes términos. 
-               <ol type="a">
-                 <br/><li >El plazo de devolución del artículo es de tres (3) días contados a partir de la recepción del envío del mismo. </li><br/>
-                 <li>Previo a la devolución, deberás enviar un correo  a <a href="mailto:devoluciones@personaling.com"><em>devoluciones@personaling.com</em></a> indicando la razón por la cual devuelves tu pedido y el número de seguimiento del mismo para el control de la empresa. Si el motivo de la devolución fuese daño a la prenda deberás anexar una foto explícita de la prenda dañada.  </li><br/>
-                 <li>El producto devuelto deberá estar en perfectas condiciones de venta con sus etiquetas y en su caja  sin haber sido usado, a menos que sea el caso del apartado “b”.  </li><br/>
-                 <li>El costo de re-envío corre por cuenta del cliente. Salvo que la devolución se deba a responsabilidad de <a href="http://www.personaling.com" title="Personaling, tu personal shopper digital">Personaling.com</a>, en cuyo caso la empresa correrá con dichos gastos y repondrá el artículo u ofrecerá un vale por el monto de la compra más el monto del envio, que podrás aplicar en posteriores compras. </li><br/>
-                 <li>El envío únicamente corre por cuenta de la compañía cuando se ha cometido un error en cuanto a la talla o si te enviamos el pedido equivocado.</li><br/>
-                 <li>Al momento de devolver el pedido debes cuidar enviarlo  en las mismas condiciones de empaque y embalaje en que fue recibido a fin de que esté protegido de daño.  </li>
-               </ol>
-             </li> <br/>
-             <li>Personaling.com no realizará devoluciones de dinero bajo ningún concepto. A beneficio de nuestros clientes entregaremos un vale de consumo por el monto del pedido devuelto que tiene validez por un año a partir de la fecha de expedición.  </li><br/>
-             <li>Personaling.com se compromete a la revisión detallada de cada uno de sus pedidos antes de la salida de su almacén; esforzándonos por un servicio certero y de calidad.</li><br/>
-             <li>Otras condiciones aplican.</li>
-           </ol>',
+
      'Copy Formas de Pago' => '        
         <div class="page-header">
           <h1>Formas de Pago: Personaling</h1>

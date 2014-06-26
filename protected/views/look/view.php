aea83c189da98a18d9cd8e22350fbe35714b1136
Yondri Roa <yroa@upsidecorp.ch>
2014-06-26 10:32:55 -0430

    Arreglos en precios y descuentos de looks

--- protected/views/look/view.php
+++ protected/views/look/view.php
@@ -128,15 +128,15 @@ $this->pageTitle=Yii::app()->name . " - " . $model->title;;
           <div class="row-fluid call2action">
             <div class="span6">
                <?php
-                if($model->getPrecioDescuento(false) < $model->getPrecio(false)){
+                if(!is_null($model->tipoDescuento) && $model->valorDescuento > 0){
                   ?>
-                  <h4 class="precio" ><div id="price"><span><?php echo Yii::t('contentForm' , 'Look Completo'); ?></span><?php echo Yii::t('contentForm', 'currSym').' '.$model->getPrecioDescuento(); ?></div></h4>
-                  <h5 class="precio" ><small><div id="price"><span><?php echo Yii::t('contentForm' , 'Por Separado'); ?></span><?php echo Yii::t('contentForm', 'currSym').' '.$model->getPrecio(); ?></div></small></h5>
+                  <h4 class="precio" ><div id="price"><span><?php echo Yii::t('contentForm' , 'Look Completo'); ?></span><?php echo Yii::t('contentForm', 'currSym').' '.$model->getPrecioTotal(); ?></div></h4>
+                  <h5 class="precio" ><small><div id="price"><span><?php echo Yii::t('contentForm' , 'Por Separado'); ?></span><?php echo Yii::t('contentForm', 'currSym').' '.$model->getPrecioProductosFull(); ?></div></small></h5>
                   
                   <?php
                 }else{
                   ?>
-                  <h4 class="precio" ><div id="price"><span><?php echo Yii::t('contentForm' , 'Subtotal'); ?></span><?php echo Yii::t('contentForm', 'currSym').' '.$model->getPrecio(); ?></div></h4>
+                  <h4 class="precio" ><div id="price"><span><?php echo Yii::t('contentForm' , 'Subtotal'); ?></span><?php echo Yii::t('contentForm', 'currSym').' '.$model->getPrecioTotal(); ?></div></h4>
                   
                   <?php 
                   

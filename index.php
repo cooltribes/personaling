0435cc09bd6cbace05f9c8528334757b6f808b4d
ranpaco <rpalma@upsidecorp.ch>
2014-11-19 11:14:39 -0430

    change debug and trace

--- index.php
+++ index.php
@@ -1,28 +1,31 @@
 <?php
 
 // change the following paths if necessary
-/*$yii=dirname(__FILE__).'/../../yii-1.1.13.e9e4a0/framework/yii.php';
+//$yii=dirname(__FILE__).'/../../yii-1.1.13.e9e4a0/framework/yii.php';
 
-	if (strstr($_SERVER['HTTP_HOST'],"personaling.es")) 
-		$config=dirname(__FILE__).'/protected/config/esp.php';
-	else if  (strstr($_SERVER['HTTP_HOST'],"personaling.com.ve"))
-	    $config=dirname(__FILE__).'/protected/config/ve.php';
-	else
-	    $config=dirname(__FILE__).'/protected/config/main.php';
+//	if (strstr($_SERVER['HTTP_HOST'],"personaling.es"))
+//		$config=dirname(__FILE__).'/protected/config/esp.php';
+//	else if  (strstr($_SERVER['HTTP_HOST'],"personaling.com.ve"))
+//	    $config=dirname(__FILE__).'/protected/config/ve.php';
+//	else
+//	    $config=dirname(__FILE__).'/protected/config/main.php';
 
-$config=dirname(__FILE__).'/protected/config/ve.php';
+//$config=dirname(__FILE__).'/protected/config/ve.php';
 // remove the following lines when in production mode
-defined('YII_DEBUG') or define('YII_DEBUG',true);
+//defined('YII_DEBUG') or define('YII_DEBUG',true);
 // specify how many levels of call stack should be shown in each log message
-defined('YII_TRACE_LEVEL') or define('YII_TRACE_LEVEL',3);
+//defined('YII_TRACE_LEVEL') or define('YII_TRACE_LEVEL',3);
 
-require_once($yii);
+//require_once($yii);
 
-Yii::createWebApplication($config)->run();*/
+//Yii::createWebApplication($config)->run();
 
 //RAFA
 // Change the following paths if necessary
-	if (strstr($_SERVER['HTTP_HOST'],"personaling.es")) 
+//defined('YII_DEBUG') or define('YII_DEBUG',true);
+//defined('YII_TRACE_LEVEL') or define('YII_TRACE_LEVEL',3);
+
+	if (strstr($_SERVER['HTTP_HOST'],"personaling.es"))
 		$country='es_es';
 	else if  (strstr($_SERVER['HTTP_HOST'],"personaling.com.ve"))
 	    $country='es_ve';
@@ -54,4 +57,5 @@ Yii::createWebApplication($config)->run();*/
 //$environment = new Environment(Environment::DEVELOPMENT,$country,'/rpalma/sandbox/personaling');
     defined('YII_DEBUG') or define('YII_DEBUG',$environment->getDebug());
     defined('YII_TRACE_LEVEL') or define('YII_TRACE_LEVEL', $environment->getTraceLevel());
+
     Yii::createWebApplication($environment->getConfig())->run();

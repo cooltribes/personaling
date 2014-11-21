6a43bf0817dccf72ace05631b61eb6f39f39a8ce
Daniel Duque <dduque@upsidecorp.ch>
2014-11-21 17:48:50 -0430

    Change parameter on facebook script

--- protected/views/tienda/look.php
+++ protected/views/tienda/look.php
@@ -25,7 +25,7 @@ if(isset($_GET['fb']) && $_GET['fb'] == 'true'){
     var ref = document.getElementsByTagName('script')[0];
     ref.parentNode.insertBefore(fpw, ref);
     })();
-    ", CClientScript::POS_HEAD, 1);
+    ", CClientScript::POS_HEAD, array()); 
 } 
 $user = User::model()->findByPk(Yii::app()->user->id);
 $status_register = -1;

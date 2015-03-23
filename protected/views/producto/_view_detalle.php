c51b122c91abc3cb041d40f66b070f9fd1763fc8
Wolfredgit config --global username Wolfredgit config --global user.name Wolfred <wmontilla@upsidecorp.ch>
2015-03-23 17:18:53 -0430

    fix facebook's problem (token)

--- protected/views/producto/_view_detalle.php
+++ protected/views/producto/_view_detalle.php
@@ -1622,9 +1622,10 @@ $('.imagen_principal').zoom({url: imgZ});
 <?php } ?>
 
 var ruta= "<?php echo Yii::app()->getBaseUrl(true);?>";
+var token= "<?php echo Yii::app()->params['fb_appId']; ?>";
 window.fbAsyncInit = function(){
     FB.init({
-        appId: '323808071078482', status: true, cookie: true, xfbml: true }); 
+        appId: token, status: true, cookie: true, xfbml: true }); 
   };
   (function(d, debug){var js, id = 'facebook-jssdk', ref = d.getElementsByTagName('script')[0];if   (d.getElementById(id)) {return;}js = d.createElement('script'); js.id = id; js.async = true;js.src = "//connect.facebook.net/es_ES/all" + (debug ? "/debug" : "") + ".js";ref.parentNode.insertBefore(js, ref);}(document, /*debug*/ false));
   function postToFeed(title, desc, url, image){
